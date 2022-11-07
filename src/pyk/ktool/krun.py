4a87a4e3db640ce85d183a440d8e71b3f7149955
Tamas Toth <tamas.toth@runtimeverification.com>
2022-11-07 11:19:10 +0100

    Add function _krunx

--- src/pyk/ktool/krun.py
+++ src/pyk/ktool/krun.py
@@ -1,11 +1,12 @@
 import json
 import logging
+from enum import Enum
 from pathlib import Path
 from subprocess import CalledProcessError, CompletedProcess
 from tempfile import NamedTemporaryFile
-from typing import Final, Iterable, Optional
+from typing import Final, Iterable, List, Mapping, Optional
 
-from ..cli_utils import check_file_path, run_process
+from ..cli_utils import check_dir_path, check_file_path, run_process
 from ..cterm import CTerm
 from ..kast import KAst, KInner, KSort
 from ..kore.parser import KoreParser
@@ -104,3 +105,95 @@ class KRun(KPrint):
             result_kast = self.kore_to_kast(result_kore)
             assert isinstance(result_kast, KInner)
             return CTerm(result_kast)
+
+
+class KRunOutput(Enum):
+    PRETTY = 'pretty'
+    PROGRAM = 'program'
+    KAST = 'kast'
+    BINARY = 'binary'
+    JSON = 'json'
+    LATEX = 'latex'
+    KORE = 'kore'
+    NONE = 'none'
+
+
+def _krunx(
+    command: str = 'krun',
+    *,
+    input_file: Optional[Path] = None,
+    definition_dir: Optional[Path] = None,
+    output: Optional[KRunOutput] = None,
+    parser: Optional[str] = None,
+    depth: Optional[int] = None,
+    pmap: Optional[Mapping[str, str]] = None,
+    cmap: Optional[Mapping[str, str]] = None,
+    term: bool = False,
+    no_expand_macros: bool = False,
+    # ---
+    check: bool = True,
+    profile: bool = True,
+) -> CompletedProcess:
+    if input_file:
+        check_file_path(input_file)
+
+    if definition_dir:
+        check_dir_path(definition_dir)
+
+    if depth and depth < 0:
+        raise ValueError(f'Expected non-negative depth, got: {depth}')
+
+    args = _build_arg_list(
+        command=command,
+        input_file=input_file,
+        definition_dir=definition_dir,
+        output=output,
+        parser=parser,
+        depth=depth,
+        pmap=pmap,
+        cmap=cmap,
+        term=term,
+        no_expand_macros=no_expand_macros,
+    )
+
+    try:
+        return run_process(args, logger=_LOGGER, check=check, profile=profile)
+    except CalledProcessError as err:
+        raise RuntimeError(
+            f'Command krun exited with code {err.returncode} for: {input_file}', err.stdout, err.stderr
+        ) from err
+
+
+def _build_arg_list(
+    *,
+    command: str,
+    input_file: Optional[Path],
+    definition_dir: Optional[Path],
+    output: Optional[KRunOutput],
+    parser: Optional[str],
+    depth: Optional[int],
+    pmap: Optional[Mapping[str, str]],
+    cmap: Optional[Mapping[str, str]],
+    term: bool,
+    no_expand_macros: bool,
+) -> List[str]:
+    args = [command]
+    if input_file:
+        args += [str(input_file)]
+    if definition_dir:
+        args += ['--definition', str(definition_dir)]
+    if output:
+        args += ['--output', output.value]
+    if parser:
+        args += ['--parser', parser]
+    if depth:
+        args += ['--depth', str(depth)]
+    for name, value in (pmap or {}).items():
+        args += [f'--p{name}={value}']
+    for name, value in (cmap or {}).items():
+        args += [f'--c{name}={value}']
+    if term:
+        args += ['--term']
+    if no_expand_macros:
+        args += ['--no-expand-macros']
+    return args
