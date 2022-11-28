9478dddf9c2d24b8fc71eace98101291108a9e4d
Tamas Toth <tamas.toth@runtimeverification.com>
2022-11-28 21:08:53 +0100

    Port to pytest: test_kprint

--- src/integration_tests/test_kprint.py
+++ src/integration_tests/test_kprint.py
@@ -1,28 +1,43 @@
-from pyk.kast.inner import KApply, KSequence, KToken, KVariable
+from pathlib import Path
+from typing import Final
+
+import pytest
+
+from pyk.kast.inner import KApply, KInner, KSequence, KToken, KVariable
 from pyk.kast.manip import remove_attrs
-from pyk.ktool.kprint import SymbolTable
+from pyk.ktool import KPrint
 from pyk.prelude.kint import intToken
 
-from .kprove_test import KProveTest
+from .utils import Kompiler
 
 
-class ImpParseTest(KProveTest):
-    KOMPILE_MAIN_FILE = 'k-files/imp.k'
+@pytest.fixture(scope='module')
+def imp_definition_dir(kompile: Kompiler) -> Path:
+    return kompile('k-files/imp.k')
 
-    @staticmethod
-    def _update_symbol_table(symbol_table: SymbolTable) -> None:
-        pass
 
-    def test_parse_token(self) -> None:
-        test_parses = (
-            ('int-token', False, KToken('3', 'Int'), intToken(3)),
-            ('id-token', False, KToken('abc', 'Id'), KToken('abc', 'Id')),
-            ('add-aexp', False, KToken('3 + 4', 'AExp'), KApply('_+_', [intToken(3), intToken(4)])),
-            ('add-int', True, KToken('3 +Int V', 'Int'), KApply('_+Int_', [intToken(3), KVariable('V')])),
-            ('k-cell', True, KToken('<k> . </k>', 'KCell'), KApply('<k>', KSequence())),
-        )
+TEST_DATA: Final = (
+    ('int-token', False, KToken('3', 'Int'), intToken(3)),
+    ('id-token', False, KToken('abc', 'Id'), KToken('abc', 'Id')),
+    ('add-aexp', False, KToken('3 + 4', 'AExp'), KApply('_+_', [intToken(3), intToken(4)])),
+    ('add-int', True, KToken('3 +Int V', 'Int'), KApply('_+Int_', [intToken(3), KVariable('V')])),
+    ('k-cell', True, KToken('<k> . </k>', 'KCell'), KApply('<k>', KSequence())),
+)
 
-        for name, as_rule, token, kast in test_parses:
-            with self.subTest(name):
-                actual_kast = self.kprove.parse_token(token, as_rule=as_rule)
-                self.assertEqual(remove_attrs(actual_kast), kast)
+
+@pytest.mark.parametrize('test_id,as_rule,token,expected', TEST_DATA, ids=[test_id for test_id, _, _, _ in TEST_DATA])
+def test_parse_token(
+    imp_definition_dir: Path,
+    test_id: str,
+    as_rule: bool,
+    token: KToken,
+    expected: KInner,
+) -> None:
+    # Given
+    kprint = KPrint(imp_definition_dir)
+
+    # When
+    actual = kprint.parse_token(token, as_rule=as_rule)
+
+    # Then
+    assert remove_attrs(actual) == expected
