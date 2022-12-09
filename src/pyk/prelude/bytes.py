f427744484b0f8f952a5b58501833937b532bb51
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-12-09 06:20:33 +0000

    Revert "prelude/bytes: add method to enquote all bytes tokens"
    This reverts commit d72be7212e8b43afe36b8460ee9daaa28252c54d.

--- src/pyk/prelude/bytes.py
+++ src/pyk/prelude/bytes.py
@@ -1,23 +1,9 @@
 from typing import Final
 
-from ..kast.inner import KInner, KSort, KToken, bottom_up
-from ..utils import enquote_str
+from ..kast.inner import KSort, KToken
 
 BYTES: Final = KSort('Bytes')
 
 
 def bytesToken(s: str) -> KToken:  # noqa: N802
     return KToken(f'b"{s}"', BYTES)
-
-
-def enquote_bytes(kast: KInner) -> KInner:
-    def _enquote_bytes(_kast: KInner) -> KInner:
-        if type(_kast) is KToken and _kast.sort == BYTES:
-            assert len(_kast.token) >= 3
-            assert _kast.token[0:2] == 'b"'
-            assert _kast.token[-1] == '"'
-            new_token = enquote_str(_kast.token[2:-1])
-            return bytesToken(new_token)
-        return _kast
-
-    return bottom_up(_enquote_bytes, kast)
