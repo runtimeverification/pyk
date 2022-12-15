b226e8789e129fe99f5b06899708189de2969ffa
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-12-15 05:52:25 +0000

    kcfg_viewer/: simpler view

--- src/pyk/kcfg_viewer/app.py
+++ src/pyk/kcfg_viewer/app.py
@@ -1,227 +1,67 @@
 import json
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, List, Optional, Set, Tuple, Union, final
+from typing import Union
 
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
-from textual.widgets import Header, Static, Tree, TreeNode
+from textual.widgets import Header, Static
+
+from pyk.kast.manip import minimize_term
+from pyk.ktool import KPrint
 
 from ..cli_utils import check_file_path
 from ..kcfg import KCFG
-from ..utils import shorten_hash
 
 
-class Terminal(ABC):
-    @property
-    @abstractmethod
-    def label(self) -> str:
-        ...
-
-
-@final
-@dataclass(frozen=True)
-class Loop(Terminal):
-    @property
-    def label(self) -> str:
-        return 'loop back'
-
-
-@final
-@dataclass(frozen=True)
-class Stuck(Terminal):
-    @property
-    def label(self) -> str:
-        return 'stuck'
-
-
-@final
-@dataclass(frozen=True)
-class Cover(Terminal):
-    cover: KCFG.Cover
-
-    @property
-    def label(self) -> str:
-        short_hash = shorten_hash(self.cover.target.id)
-        return f'covered by {short_hash}'
-
-
-class NodeModel(ABC):
-    @property
-    @abstractmethod
-    def node(self) -> KCFG.Node:
-        ...
-
-    @property
-    @abstractmethod
-    def terminal(self) -> Optional[Terminal]:
-        ...
-
-    @property
-    def label(self) -> str:
-        short_hash = shorten_hash(self.node.id)
-        if self.terminal:
-            return f'{short_hash} ({self.terminal.label})'
-        return short_hash
-
-
-@final
-@dataclass(frozen=True)
-class Init(NodeModel):
-    _node: KCFG.Node
-
-    def __init__(self, node: KCFG.Node):
-        object.__setattr__(self, '_node', node)
-
-    @property
-    def node(self) -> KCFG.Node:
-        return self._node
-
-    @property
-    def terminal(self) -> None:
-        return None
-
-    def __str__(self) -> str:
-        return f'[Init] {self.label}'
-
-
-@final
-@dataclass(frozen=True)
-class Step(NodeModel):
-    in_edge: KCFG.Edge
-    _terminal: Optional[Terminal] = None
-
-    @property
-    def node(self) -> KCFG.Node:
-        return self.in_edge.target
-
-    @property
-    def terminal(self) -> Optional[Terminal]:
-        return self._terminal
-
-    def __str__(self) -> str:
-        return f'[Step {self.in_edge.depth}] {self.label}'
-
-
-@final
-@dataclass(frozen=True)
-class Branch(NodeModel):
-    in_edge: KCFG.Edge
-    _terminal: Optional[Terminal] = None
-
-    @property
-    def node(self) -> KCFG.Node:
-        return self.in_edge.target
-
-    @property
-    def terminal(self) -> Optional[Terminal]:
-        return self._terminal
-
-    def __str__(self) -> str:
-        return f'[Branch] {self.label}'
-
-
-@final
-@dataclass(frozen=True)
-class Case(NodeModel):
-    cover: KCFG.Cover
-    _terminal: Optional[Terminal] = None
-
-    @property
-    def node(self) -> KCFG.Node:
-        return self.cover.target
-
-    @property
-    def terminal(self) -> Optional[Terminal]:
-        return self._terminal
-
-    def __str__(self) -> str:
-        return f'[Case] {self.label}'
-
-
-def cfg_tree(cfg: KCFG, label: str, id: str) -> Tree[NodeModel]:
-    tree: Tree[NodeModel] = Tree(label, id=id)
-    root = tree.root
-
-    indent_model = {Init, Branch, Case}
-
-    waitlist: List[Tuple[TreeNode, NodeModel]] = [(root, Init(init)) for init in cfg.init]
-    visited: Set[str] = set()
-    while waitlist:
-        parent, model = waitlist.pop(0)  # BFS
-
-        allow_expand = type(model) in indent_model and not model.terminal
-        current = parent.add(label=str(model), data=model, allow_expand=allow_expand)
-
-        if model.terminal:
-            continue
-
-        next_parent = current if type(model) in indent_model else parent
-
-        out_edges = cfg.edges(source_id=model.node.id)
-        next_branch = len(out_edges) > 1
-        for out_edge in out_edges:
-            next_node = out_edge.target
-            terminal: Optional[Terminal]
-            if cfg.is_stuck(next_node.id):
-                terminal = Stuck()
-            elif cfg.is_covered(next_node.id):
-                cover = cfg.covers(source_id=next_node.id)[0]
-                terminal = Cover(cover)
-            elif next_node.id in visited:
-                terminal = Loop()
-            else:
-                terminal = None
-
-            next_model: NodeModel
-            if next_branch:
-                next_model = Branch(in_edge=out_edge, _terminal=terminal)
-            else:
-                next_model = Step(in_edge=out_edge, _terminal=terminal)
-
-            waitlist.append((next_parent, next_model))
-            visited.add(next_node.id)
-
-    return tree
-
-
-class KcfgViewer(App):
+class KCFGViewer(App):
     CSS_PATH = 'style.css'
 
     _kcfg_file: Path
     _cfg: KCFG
-    _printer: Callable[[KCFG, KCFG.Node], str]
+    _kprint: KPrint
+    _curr_node: str
+    _minimize: bool
 
-    def __init__(self, kcfg_file: Union[str, Path], printer: Optional[Callable[[KCFG, KCFG.Node], str]] = None) -> None:
+    BINDINGS = [
+        ('p', 'select_node("prev")', 'Select previous node'),
+        ('n', 'select_node("next")', 'Select next node'),
+        ('P', 'select_node("Prev")', 'Select prev branch'),
+        ('N', 'select_node("Next")', 'Select next branch'),
+    ]
+
+    def __init__(self, kcfg_file: Union[str, Path], kprint: KPrint) -> None:
         kcfg_file = Path(kcfg_file)
         check_file_path(kcfg_file)
         super().__init__()
         self._kcfg_file = kcfg_file
         self._cfg = KCFG.from_dict(json.loads(kcfg_file.read_text()))
-        self._printer = printer if printer else lambda cfg, node: '\n'.join(cfg.node_short_info(node))
+        self._kprint = kprint
+        self._curr_node = self._cfg.get_unique_init().id
+        self._minimize = True
+
+    def _behavior_text(self) -> str:
+        text_lines = self._cfg.pretty(self._kprint, minimize=self._minimize)
+        return '\n'.join(text_lines)
+
+    def _node_text(self) -> str:
+        kast = self._cfg.node(self._curr_node).cterm.kast
+        if self._minimize:
+            kast = minimize_term(kast)
+        return self._kprint.pretty_print(kast)
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Horizontal(
-            cfg_tree(self._cfg, label=str(self._kcfg_file), id='tree-view'),
-            Vertical(
-                Static(id='text', expand=True),
-                id='text-view',
-            ),
+            Vertical(Static(self._behavior_text(), id='behavior'), id='behavior-view'),
+            Vertical(Static(self._node_text(), id='node'), id='node-view'),
         )
 
-    def display_node(self, node: TreeNode) -> None:
-        model = node.data
-        static = self.query_one('#text', Static)
-
-        if not model:
-            static.update('')
-            return
-
-        text = self._printer(self._cfg, model.node)
-        static.update(text)
-
-    def on_tree_node_selected(self, event: Tree.NodeSelected) -> None:
-        node = event.node
-        self.display_node(node)
+    def action_select_node(self, nid: str) -> None:
+        if nid == 'next':
+            edges = self._cfg.edges(source_id=self._curr_node)
+            if len(edges) == 1:
+                self._curr_node = edges[0].target.id
+        elif nid == 'prev':
+            edges = self._cfg.edges(target_id=self._curr_node)
+            if len(edges) == 1:
+                self._curr_node = edges[0].source.id
