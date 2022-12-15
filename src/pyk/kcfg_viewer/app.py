4bc023d0db8a2d2b459ace8d2a2415164656e070
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-12-15 06:33:48 +0000

    kcfg_viewer: basic navigation

--- src/pyk/kcfg_viewer/app.py
+++ src/pyk/kcfg_viewer/app.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from typing import Union
+from typing import Dict, List, Tuple, Union
 
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
@@ -11,6 +11,7 @@ from pyk.ktool import KPrint
 
 from ..cli_utils import check_file_path
 from ..kcfg import KCFG
+from ..utils import shorten_hashes
 
 
 class KCFGViewer(App):
@@ -23,10 +24,18 @@ class KCFGViewer(App):
     _minimize: bool
 
     BINDINGS = [
-        ('p', 'select_node("prev")', 'Select previous node'),
-        ('n', 'select_node("next")', 'Select next node'),
-        ('P', 'select_node("Prev")', 'Select prev branch'),
-        ('N', 'select_node("Next")', 'Select next branch'),
+        ('p', 'select_node("p")', 'Select previous node'),
+        ('n', 'select_node("n")', 'Select next node'),
+        ('0', 'select_node("0")', 'Select node 0'),
+        ('1', 'select_node("1")', 'Select node 1'),
+        ('2', 'select_node("2")', 'Select node 2'),
+        ('3', 'select_node("3")', 'Select node 3'),
+        ('4', 'select_node("4")', 'Select node 4'),
+        ('5', 'select_node("5")', 'Select node 5'),
+        ('6', 'select_node("6")', 'Select node 6'),
+        ('7', 'select_node("7")', 'Select node 7'),
+        ('8', 'select_node("8")', 'Select node 8'),
+        ('9', 'select_node("9")', 'Select node 9'),
     ]
 
     def __init__(self, kcfg_file: Union[str, Path], kprint: KPrint) -> None:
@@ -38,6 +47,43 @@ class KCFGViewer(App):
         self._kprint = kprint
         self._curr_node = self._cfg.get_unique_init().id
         self._minimize = True
+
+    def _navigation_options(self) -> Dict[str, Tuple[str, str]]:
+        nav_opts = {}
+        in_edges: List[KCFG.EdgeLike] = []
+        out_edges: List[KCFG.EdgeLike] = []
+        for e in self._cfg.edges(target_id=self._curr_node):
+            in_edges.append(e)
+        for c in self._cfg.covers(target_id=self._curr_node):
+            in_edges.append(c)
+        for e in self._cfg.edges(source_id=self._curr_node):
+            out_edges.append(e)
+        for c in self._cfg.covers(source_id=self._curr_node):
+            out_edges.append(c)
+        counter = 0
+        if len(in_edges) == 1:
+            nav_opts['p'] = ('prev', in_edges[0].source.id)
+        else:
+            for ie in in_edges:
+                if counter > 9:
+                    break
+                nav_opts[str(counter)] = ('prev', ie.source.id)
+                counter += 1
+        if len(out_edges) == 1:
+            nav_opts['n'] = ('next', out_edges[0].target.id)
+        else:
+            for oe in out_edges:
+                if counter > 9:
+                    break
+                nav_opts[str(counter)] = ('next', oe.target.id)
+                counter += 1
+        return nav_opts
+
+    def _navigation_text(self) -> str:
+        text_lines = [f'current node: {shorten_hashes(self._curr_node)}']
+        for k, (d, n) in self._navigation_options().items():
+            text_lines.append(f'    {k}: {d} {shorten_hashes(n)}')
+        return '\n'.join(text_lines)
 
     def _behavior_text(self) -> str:
         text_lines = self._cfg.pretty(self._kprint, minimize=self._minimize)
@@ -52,17 +98,22 @@ class KCFGViewer(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Horizontal(
-            Vertical(Static(self._behavior_text(), id='behavior'), id='behavior-view'),
-            Vertical(Static(self._node_text(), id='node'), id='node-view'),
+            Vertical(
+                Horizontal(Static(self._navigation_text(), id='navigation'), id='navigation-view'),
+                Horizontal(Static(self._behavior_text(), id='behavior'), id='behavior-view'),
+                id='left-pane-view',
+            ),
+            Vertical(Static(self._node_text(), id='node'), id='right-pane-view'),
         )
 
-    def action_select_node(self, nid: str) -> None:
-        if nid == 'next':
-            edges = self._cfg.edges(source_id=self._curr_node)
-            if len(edges) == 1:
-                self._curr_node = edges[0].target.id
-        elif nid == 'prev':
-            edges = self._cfg.edges(target_id=self._curr_node)
-            if len(edges) == 1:
-                self._curr_node = edges[0].source.id
+    def update(self) -> None:
+        self.query_one('#navigation', Static).update(self._navigation_text())
+        # self.query_one('#behavior', Static).update(self._behavior_text())
         self.query_one('#node', Static).update(self._node_text())
+
+    def action_select_node(self, nid: str) -> None:
+        nav_opts = self._navigation_options()
+        if nid in nav_opts:
+            _, next_node = nav_opts[nid]
+            self._curr_node = next_node
+            self.update()
