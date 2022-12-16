5424290706ba328bfb8e8b984db7714f0a167156
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-12-16 18:57:06 +0000

    kcfg_viewer/: first attempt at toggleable views

--- src/pyk/kcfg_viewer/app.py
+++ src/pyk/kcfg_viewer/app.py
@@ -1,11 +1,13 @@
 import json
 from pathlib import Path
-from typing import Dict, Iterable, List, Optional, Tuple, Union
+from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
+from textual.reactive import var
 from textual.widgets import Header, Static
 
+from pyk.cterm import CTerm
 from pyk.kast.inner import KApply
 from pyk.kast.manip import minimize_term
 from pyk.ktool import KPrint
@@ -23,7 +25,13 @@ class KCFGViewer(App):
     _cfg: KCFG
     _kprint: KPrint
     _curr_node: str
+    _node_printer: Optional[Callable[[CTerm], Iterable[str]]]
+    _custom_printer: Optional[Callable[[CTerm], Iterable[str]]]
     _minimize: bool
+
+    _term_view = var(True)
+    _constraint_view = var(True)
+    _user_view = var(False)
 
     BINDINGS = [
         ('p', 'keystroke("p")', 'Select previous node'),
@@ -38,10 +46,19 @@ class KCFGViewer(App):
         ('7', 'keystroke("7")', 'Select node 7'),
         ('8', 'keystroke("8")', 'Select node 8'),
         ('9', 'keystroke("9")', 'Select node 9'),
-        ('m', 'keystroke("m")', 'Toggle node minimization'),
+        ('m', 'keystroke("m")', 'Toggle term minimization'),
+        ('t', 'keystroke("t")', 'Toggle term view'),
+        ('c', 'keystroke("c")', 'Toggle constraint view'),
+        ('u', 'keystroke("u")', 'Toggle user supplied view'),
     ]
 
-    def __init__(self, kcfg_file: Union[str, Path], kprint: KPrint) -> None:
+    def __init__(
+        self,
+        kcfg_file: Union[str, Path],
+        kprint: KPrint,
+        node_printer: Optional[Callable[[CTerm], Iterable[str]]] = None,
+        custom_printer: Optional[Callable[[CTerm], Iterable[str]]] = None,
+    ) -> None:
         kcfg_file = Path(kcfg_file)
         check_file_path(kcfg_file)
         super().__init__()
@@ -49,7 +66,12 @@ class KCFGViewer(App):
         self._cfg = KCFG.from_dict(json.loads(kcfg_file.read_text()))
         self._kprint = kprint
         self._curr_node = self._cfg.get_unique_init().id
+        self._node_printer = node_printer
+        self._custom_printer = custom_printer
         self._minimize = True
+        self._term_view = True
+        self._constraint_view = True
+        self._user_view = False
 
     def _navigation_options(self) -> Dict[str, Tuple[str, str]]:
         nav_opts = {}
@@ -89,21 +111,28 @@ class KCFGViewer(App):
         return '\n'.join(text_lines)
 
     def _behavior_text(self) -> str:
-        text_lines = self._cfg.pretty(self._kprint, minimize=self._minimize)
+        text_lines = self._cfg.pretty(self._kprint, minimize=self._minimize, node_printer=self._node_printer)
         return '\n'.join(text_lines)
 
     def _display_text(self) -> str:
         text_lines = ['display options:']
         text_lines.append(f'    m: toggle minimization: {self._minimize}')
+        text_lines.append(f'    t: toggle term view: {self._term_view}')
+        text_lines.append(f'    c: toggle constraint view: {self._constraint_view}')
+        text_lines.append(f'    u: toggle user view: {self._user_view}')
         return '\n'.join(text_lines)
 
-    def _node_text(self) -> str:
+    def _term_text(self) -> str:
+        if not self._term_view:
+            return ''
         kast = self._cfg.node(self._curr_node).cterm.config
         if self._minimize:
             kast = minimize_term(kast)
         return self._kprint.pretty_print(kast)
 
     def _constraint_text(self) -> str:
+        if not self._constraint_view:
+            return ''
         constraints = self._cfg.node(self._curr_node).cterm.constraints
         text_lines = []
         for c in constraints:
@@ -115,8 +144,22 @@ class KCFGViewer(App):
                 text_lines.append(self._kprint.pretty_print(c))
         return '\n'.join(text_lines)
 
+    def _user_text(self) -> str:
+        if not self._user_view:
+            return ''
+        if self._custom_printer is None:
+            return 'No custom_printer supplied!'
+        return '\n'.join(self._custom_printer(self._cfg.node(self._curr_node).cterm))
+
     def compose(self) -> ComposeResult:
         yield Header()
+        right_pane_views = [Horizontal(Static(id='display'), id='display-view')]
+        if self._term_view:
+            right_pane_views.append(Horizontal(Static(id='term'), id='term-view'))
+        if self._constraint_view:
+            right_pane_views.append(Horizontal(Static(id='constraint'), id='constraint-view'))
+        if self._user_view:
+            right_pane_views.append(Horizontal(Static(id='user'), id='user-view'))
         yield Horizontal(
             Vertical(
                 Horizontal(Static(id='navigation'), id='navigation-view'),
@@ -124,9 +167,7 @@ class KCFGViewer(App):
                 id='left-pane-view',
             ),
             Vertical(
-                Horizontal(Static(id='display'), id='display-view'),
-                Horizontal(Static(id='node'), id='node-view'),
-                Horizontal(Static(id='constraint'), id='constraint-view'),
+                *right_pane_views,
                 id='right-pane-view',
             ),
         )
@@ -138,8 +179,8 @@ class KCFGViewer(App):
             self.query_one('#behavior', Static).update(self._behavior_text())
         if components is None or 'display' in components:
             self.query_one('#display', Static).update(self._display_text())
-        if components is None or 'node' in components:
-            self.query_one('#node', Static).update(self._node_text())
+        if components is None or 'term' in components:
+            self.query_one('#term', Static).update(self._term_text())
         if components is None or 'constraint' in components:
             self.query_one('#constraint', Static).update(self._constraint_text())
 
@@ -151,7 +192,16 @@ class KCFGViewer(App):
         if nid in nav_opts:
             _, next_node = nav_opts[nid]
             self._curr_node = next_node
-            self.update(['navigation', 'node', 'constraint'])
+            self.update(['navigation', 'term', 'constraint'])
         elif nid == 'm':
             self._minimize = not self._minimize
-            self.update(['display', 'node'])
+            self.update(['display', 'term'])
+        elif nid == 't':
+            self._term_view = not self._term_view
+            self.update(['display', 'term'])
+        elif nid == 'c':
+            self._constraint_view = not self._constraint_view
+            self.update(['display', 'constraint'])
+        elif nid == 'u':
+            self._user_view = not self._user_view
+            self.update(['display', 'user'])
