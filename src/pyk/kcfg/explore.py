7c0095dd6e3fb3ca352186a0961cfca210564a88
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-01-30 23:29:35 +0000

    Revert "kcfg/explore: allow repeating step exploration"
    This reverts commit 591a5e7b9c7348362add18445dbc254955c6a080.

--- src/pyk/kcfg/explore.py
+++ src/pyk/kcfg/explore.py
@@ -163,11 +163,7 @@ class KCFGExplore(ContextManager['KCFGExplore']):
                 cfg.replace_node(node.id, CTerm(new_term))
         return cfg
 
-    def step(self, cfgid: str, cfg: KCFG, node_id: str, repeat: int = 1) -> KCFG:
-        if repeat == 0:
-            return cfg
-        if repeat < 0:
-            raise ValueError(f'Repeat parameter must be non-negative: {repeat}')
+    def step(self, cfgid: str, cfg: KCFG, node_id: str) -> KCFG:
         node = cfg.node(node_id)
         out_edges = cfg.edges(source_id=node.id)
         if len(out_edges) > 1:
@@ -189,7 +185,7 @@ class KCFGExplore(ContextManager['KCFGExplore']):
             cfg.remove_edge(edge.source.id, edge.target.id)
             cfg.create_edge(edge.source.id, new_node.id, condition=mlTop(), depth=1)
             cfg.create_edge(new_node.id, edge.target.id, condition=edge.condition, depth=(edge.depth - 1))
-        return self.step(cfgid, cfg, new_node.id, repeat=(repeat - 1))
+        return cfg
 
     def section_edge(self, cfgid: str, cfg: KCFG, source_id: str, target_id: str, sections: int = 2) -> KCFG:
         if sections <= 1:
