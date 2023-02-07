0181bab3b2c0fc57bfa523d56db2e6be367f5c8f
Tamas Toth <tamas.toth@runtimeverification.com>
2023-02-07 11:20:08 +0100

    Move `KompiledKore` to its own module

--- src/pyk/konvert.py
+++ src/pyk/konvert.py
@@ -1,112 +1,14 @@
-from collections import defaultdict
-from dataclasses import dataclass
-from functools import cached_property, reduce
-from pathlib import Path
-from typing import Dict, Final, FrozenSet, Iterable, Optional, Set, Tuple, Union, final
+from functools import reduce
+from typing import Final, Optional, Tuple
 
-from pyk.kast.inner import KApply, KInner, KSequence, KSort, KToken, KVariable
-from pyk.kast.outer import KDefinition
-from pyk.kore.syntax import DV, App, EVar, MLPattern, MLQuant, Pattern, SortApp, String
-from pyk.prelude.bytes import BYTES
-from pyk.prelude.k import K
-from pyk.prelude.string import STRING
-
-from .cli_utils import check_dir_path, check_file_path
-from .kore.parser import KoreParser
-from .kore.syntax import Definition, Sort
+from .kast.inner import KApply, KInner, KSequence, KSort, KToken, KVariable
+from .kast.outer import KDefinition
+from .kore.kompiled import KompiledKore
+from .kore.syntax import DV, App, EVar, MLPattern, MLQuant, Pattern, Sort, SortApp, String
+from .prelude.bytes import BYTES
+from .prelude.k import K
+from .prelude.string import STRING
 from .utils import FrozenDict
-
-
-@final
-@dataclass(frozen=True)
-class KompiledKore:
-    path: Path
-    timestamp: int
-
-    def __init__(self, definition_dir: Union[str, Path]):
-        definition_dir = Path(definition_dir)
-        check_dir_path(definition_dir)
-
-        path = (definition_dir / 'definition.kore').resolve()
-        check_file_path(path)
-
-        timestamp_file = definition_dir / 'timestamp'
-        check_file_path(timestamp_file)
-        timestamp = timestamp_file.stat().st_mtime_ns
-
-        object.__setattr__(self, 'path', path)
-        object.__setattr__(self, 'timestamp', timestamp)
-
-    @cached_property
-    def definition(self) -> Definition:
-        return KoreParser(self.path.read_text()).definition()
-
-    @cached_property
-    def _subsort_table(self) -> FrozenDict[Sort, FrozenSet[Sort]]:
-        axioms = (axiom for module in self.definition for axiom in module.axioms)
-        attrs = (attr for axiom in axioms for attr in axiom.attrs)
-        subsort_attrs = (attr for attr in attrs if attr.symbol == 'subsort')
-        subsort_attr_sorts = (attr.sorts for attr in subsort_attrs)
-
-        subsort_table: Dict[Sort, Set[Sort]] = defaultdict(set)
-        for subsort, supersort in subsort_attr_sorts:
-            subsort_table[supersort].add(subsort)
-
-        return FrozenDict({supersort: frozenset(subsorts) for supersort, subsorts in subsort_table.items()})
-
-    def is_subsort(self, sort1: Sort, sort2: Sort) -> bool:
-        if sort1 == sort2:
-            return True
-
-        if sort2 == SortApp('SortK'):
-            return True
-
-        if sort1 == SortApp('SortK'):
-            return False
-
-        return sort1 in self._subsort_table.get(sort2, frozenset())
-
-    def meet_sorts(self, sort1: Sort, sort2: Sort) -> Sort:
-        if self.is_subsort(sort1, sort2):
-            return sort1
-
-        if self.is_subsort(sort2, sort1):
-            return sort2
-
-        subsorts1 = set(self._subsort_table.get(sort1, set())).union({sort1})
-        subsorts2 = set(self._subsort_table.get(sort2, set())).union({sort2})
-        common_subsorts = subsorts1.intersection(subsorts2)
-        if not common_subsorts:
-            raise ValueError(f'Sorts have no common subsort: {sort1}, {sort2}')
-        nr_subsorts = {sort: len(self._subsort_table.get(sort, {})) for sort in common_subsorts}
-        max_subsort_nr = max(n for _, n in nr_subsorts.items())
-        max_subsorts = {sort for sort, n in nr_subsorts.items() if n == max_subsort_nr}
-        (subsort,) = max_subsorts
-        return subsort
-
-    def meet_all_sorts(self, sorts: Iterable[Sort]) -> Sort:
-        unit: Sort = SortApp('SortK')
-        return reduce(self.meet_sorts, sorts, unit)
-
-    def add_injections(self, pattern: Pattern, sort: Optional[Sort] = None) -> Pattern:
-        if sort is None:
-            sort = SortApp('SortK')
-        patterns = pattern.patterns
-        sorts = self.definition.pattern_sorts(pattern)
-        pattern = pattern.let_patterns(self.add_injections(p, s) for p, s in zip(patterns, sorts))
-        return self._inject(pattern, sort)
-
-    def _inject(self, pattern: Pattern, sort: Sort) -> Pattern:
-        actual_sort = self.definition.infer_sort(pattern)
-
-        if actual_sort == sort:
-            return pattern
-
-        if self.is_subsort(actual_sort, sort):
-            return App('inj', (actual_sort, sort), (pattern,))
-
-        raise ValueError(f'Sort {actual_sort.name} is not a subsort of {sort.name}')
-
 
 # ------------
 # KAST-to-KORE
