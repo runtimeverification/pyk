9c4f290ca9aebf162ff7e8cc3b3dbe1af5a0b341
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-02-28 20:35:48 +0000

    ktool/kompile: more specific check on llmv_mode

--- src/pyk/ktool/kompile.py
+++ src/pyk/ktool/kompile.py
@@ -59,6 +59,9 @@ def kompile(
         _check_backend_param(not no_llvm_kompile, 'no_llvm_kompile', backend)
         _check_backend_param(llvm_mode is None, 'llvm_mode', backend)
 
+    if backend == KompileBackend.LLVM:
+        _check_backend_param(llvm_mode is None or llvm_mode == 'c', f'llvm_mode={llvm_mode}', backend)
+
     if backend != KompileBackend.HASKELL:
         _check_backend_param(not list(concrete_rules), 'concrete_rules', backend)
 
