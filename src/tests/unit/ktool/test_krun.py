814edb79fcbc5e46419b562f4a271afc3eb19fd8
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-09 15:07:11 -0600

    test_krun: Add test for _build_arg_list

---
+++ src/tests/unit/ktool/test_krun.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from pyk.ktool.krun import KRunOutput, _build_arg_list
+
+
+def test_all_args_1() -> None:
+    # Given
+    # fmt: off
+    expected = [
+        'kevm-krun',
+        'input/path',
+        '--definition', 'def/dir',
+        '--output', 'json',
+        '--parser', 'cat',
+        '--depth', '12355',
+        '-pFOO=bar', '-pBUZZ=kill',
+        '-cCOO=car', '-cFUZZ=bill',
+        '--term',
+        '--no-expand-macros',
+    ]
+    # fmt: on
+
+    # When
+    actual = _build_arg_list(
+        command='kevm-krun',
+        input_file=Path('input/path'),
+        definition_dir=Path('def/dir'),
+        output=KRunOutput.JSON,
+        parser='cat',
+        pmap={'FOO': 'bar', 'BUZZ': 'kill'},
+        cmap={'COO': 'car', 'FUZZ': 'bill'},
+        depth=12355,
+        term=True,
+        no_expand_macros=True,
+    )
+
+    # Then
+    assert actual == expected
