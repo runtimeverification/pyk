2e7a93b3c8f3f9b9da51376ce62251190775cb1d
Georgy Lukyanov <mail@geo2a.info>
2023-03-23 09:52:24 +0100

    Factor out log analysis, add unit tests

--- src/pyk/kore_exec_covr/__main__.py
+++ src/pyk/kore_exec_covr/__main__.py
@@ -1,30 +1,19 @@
 import logging
-import re
 from argparse import ArgumentParser
-from enum import Enum
 from pathlib import Path
-from typing import Any, Dict, Final, Tuple
+from typing import Dict, Final
 
 import coloredlogs
 
-from pyk.kast.kast import KAtt
-from pyk.kast.outer import KDefinition, KRule, read_kast_definition
-from pyk.ktool.kprint import build_symbol_table, pretty_print_kast
+from pyk.kast.outer import read_kast_definition
+from pyk.kore_exec_covr.kore_exec_covr import HaskellLogEntry, build_rule_dict, parse_rule_applications
 
 from ..cli_utils import dir_path, file_path
 
 _LOG_FORMAT: Final = '%(levelname)s %(name)s - %(message)s'
-_LOGGER: Final = logging.getLogger(__name__)
-
-_HASKELL_LOG_ENTRY_REGEXP: Final = re.compile(r'kore-exec: \[\d*\] Debug \(([a-zA-Z]*)\): (.*)')
 
 
-class HaskellLogEntry(Enum):
-    DEBUG_APPLY_EQUATION = 'DebugApplyEquation'
-    DEBUG_APPLIED_REWRITE_RULES = 'DebugAppliedRewriteRules'
-
-
-def do_analyze(definition_dir: Path, input_file: Path, **kwargs: Any) -> None:
+def do_analyze(definition_dir: Path, input_file: Path) -> None:
     """
     Inputs:
        * definition compiled with "kompile --backend haskell --emit-json"
@@ -36,113 +25,55 @@ def do_analyze(definition_dir: Path, input_file: Path, **kwargs: Any) -> None:
     """
     definition = read_kast_definition(definition_dir / 'compiled.json')
 
-    _LOGGER.info(f'Discovering rewrite and simplification rules in {definition_dir}')
+    print(f'Discovering rewrite and simplification rules in {definition_dir}')
     rule_dict = build_rule_dict(definition)
 
-    rewrites: Dict[str, int] = {key: 0 for key, rule in rule_dict.items() if not 'simplification' in rule.att.atts}
-    simplifications: Dict[str, int] = {key: 0 for key, rule in rule_dict.items() if 'simplification' in rule.att.atts}
-    _LOGGER.info(f'    Found {len(rewrites)} rewrite rules')
-    _LOGGER.info(f'    Found {len(simplifications)} simplification rules')
+    defined_rewrites: Dict[str, int] = {
+        key: 0 for key, rule in rule_dict.items() if not 'simplification' in rule.att.atts
+    }
+    defined_simplifications: Dict[str, int] = {
+        key: 0 for key, rule in rule_dict.items() if 'simplification' in rule.att.atts
+    }
+    print(f'    Found {len(defined_rewrites)} rewrite rules availible for {definition.main_module_name}')
+    print(f'    Found {len(defined_simplifications)} simplification rules availible for {definition.main_module_name}')
 
-    log_entries = input_file.read_text().splitlines()
-    for log_entry in log_entries:
-        entry_type, location_str = parse_haskell_one_line_log(log_entry)
-        # TODO: the haskell backend log files often contain items with no location. it is not clear to me what happens.
-        if location_str:
-            try:
-                if entry_type == HaskellLogEntry.DEBUG_APPLIED_REWRITE_RULES:
-                    rewrites[location_str] += 1
-                elif entry_type == HaskellLogEntry.DEBUG_APPLY_EQUATION:
-                    simplifications[location_str] += 1
-            except KeyError:
-                _LOGGER.warning(f'unknown rule location: {location_str}')
+    parsed_rule_applictions = parse_rule_applications(input_file)
+    applied_rewrites = parsed_rule_applictions[HaskellLogEntry.DEBUG_APPLIED_REWRITE_RULES]
+    applied_simplifications = parsed_rule_applictions[HaskellLogEntry.DEBUG_APPLY_EQUATION]
 
     print('=================================')
     print('=== REWRITES ====================')
     print('=================================')
-    for location_str, hits in rewrites.items():
+    for location_str, hits in applied_rewrites.items():
         rule_label_str = ''
-        if 'label' in rule_dict[location_str].att.atts:
+        if location_str in rule_dict and 'label' in rule_dict[location_str].att.atts:
             rule_label_str = f'[{rule_dict[location_str].att.atts["label"]}]'
         if hits > 0:
             print(f'    {hits} applications of rule {rule_label_str} defined at {location_str}')
-    total_rewrites = sum([v for v in rewrites.values() if v > 0])
-    print(f'Total rewrites: {total_rewrites}')
+    total_rewrites = sum([v for v in applied_rewrites.values() if v > 0])
+    print(f'Total rewrites applied: {total_rewrites}')
 
     print('=================================')
     print('=== SIMPLIFICATIONS =============')
     print('=================================')
-    for location_str, hits in simplifications.items():
+    for location_str, hits in applied_simplifications.items():
         rule_label_str = ''
-        if 'label' in rule_dict[location_str].att.atts:
+        if location_str in rule_dict and 'label' in rule_dict[location_str].att.atts:
             rule_label_str = f'[{rule_dict[location_str].att.atts["label"]}]'
         if hits > 0:
             print(f'    {hits} applications of rule {rule_label_str} defined at {location_str}')
-    total_simplifications = sum([v for v in simplifications.values() if v > 0])
-    print(f'Total simplifications: {total_simplifications}')
-
-
-def parse_haskell_one_line_log(log_entry: str) -> Tuple[HaskellLogEntry, str]:
-    """Attempt to parse a one-line log string emmitted by K's Haskell backend"""
-    matches = _HASKELL_LOG_ENTRY_REGEXP.match(log_entry)
-    try:
-        assert matches
-        entry = matches.groups()[0]
-        location_str = matches.groups()[1].strip()
-        return HaskellLogEntry(entry), location_str
-    except (AssertionError, KeyError, ValueError) as err:
-        _LOGGER.error(f'failed to parse log entry: {log_entry}')
-        raise err
-
-
-def build_rule_dict(
-    definition: KDefinition, *, skip_projections: bool = True, skip_initializers: bool = True
-) -> Dict[str, KRule]:
-    """
-    Traverse the kompiled definition and build a dictionary mapping str(file:location) to KRule
-    """
-    symbol_table = build_symbol_table(definition)
-
-    rule_dict: Dict[str, KRule] = {}
-
-    for rule in definition.rules:
-        if skip_projections and 'projection' in rule.att.atts:
-            continue
-        if skip_initializers and 'initializer' in rule.att.atts:
-            continue
-        try:
-            rule_source = rule.att.atts[KAtt.SOURCE]
-            rule_location = rule.att.atts[KAtt.LOCATION]
-        except KeyError:
-            _LOGGER.warning(
-                'Skipping rule with no location information {msg:.50}...<truncated>'.format(
-                    msg=str(pretty_print_kast(rule.body, symbol_table))
-                )
-            )
-            rule_source = None
-            rule_location = None
-            continue
-        if rule_source and rule_location:
-            rule_dict[f'{rule_source}:{_location_tuple_to_str(rule_location)}'] = rule
-        else:
-            raise ValueError(pretty_print_kast(rule.body, symbol_table))
-
-    return rule_dict
-
-
-def _location_tuple_to_str(location: Tuple[int, int, int, int]) -> str:
-    start_line, start_col, end_line, end_col = location
-    return f'{start_line}:{start_col}-{end_line}:{end_col}'
+    total_simplifications = sum([v for v in applied_simplifications.values() if v > 0])
+    print(f'Total applied simplifications: {total_simplifications}')
 
 
 def main() -> None:
-    coloredlogs.install(fmt=_LOG_FORMAT)
-    args = vars(_argument_parser().parse_args())
+    args = _argument_parser().parse_args()
 
-    level = logging.ERROR if args['quiet'] else logging.INFO
+    level = logging.ERROR if args.quiet else logging.INFO
     logging.basicConfig(level=level, format=_LOG_FORMAT)
+    coloredlogs.install(level=level, fmt=_LOG_FORMAT)
 
-    return do_analyze(**args)
+    return do_analyze(definition_dir=args.definition_dir, input_file=args.input_file)
 
 
 def _argument_parser() -> ArgumentParser:
