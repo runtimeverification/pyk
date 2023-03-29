7945f33ea47c5ad66d62ce6dd05a16a02c62e87d
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-03-29 20:28:09 +0200

    Add function `parse_text` to module `parser`

--- src/pyk/kllvm/parser.py
+++ src/pyk/kllvm/parser.py
@@ -8,12 +8,15 @@ from _kllvm.parser import Parser  # type: ignore
 from ..cli_utils import check_file_path
 
 if TYPE_CHECKING:
-    pass
-
     from .ast import Pattern
 
 
-def read_pattern(path: str | Path) -> Pattern:
+def parse_text(text: str) -> Pattern:
+    parser = Parser.from_string(text)
+    return parser.pattern()
+
+
+def parse_file(path: str | Path) -> Pattern:
     path = Path(path)
     check_file_path(path)
     parser = Parser(str(path))
