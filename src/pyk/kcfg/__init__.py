f5f6f8b7def9a8593a41d368abbad4ba77b24080
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-03-29 22:45:57 +0000

    kcfg/: move reachability module => prover

--- src/pyk/kcfg/__init__.py
+++ src/pyk/kcfg/__init__.py
@@ -1,5 +1,5 @@
 from .explore import KCFGExplore
 from .kcfg import KCFG
-from .reachability import AllPathReachabilityProof
+from .proof import AllPathReachabilityProof
 from .show import KCFGShow
 from .tui import KCFGViewer
