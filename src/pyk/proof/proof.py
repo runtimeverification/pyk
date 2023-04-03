9bb61dfa5935095ec82c35b28911ded2dea44ed8
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-04-03 20:49:11 +0000

    proof/: write proofs from new Prover superclass

--- src/pyk/proof/proof.py
+++ src/pyk/proof/proof.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+import json
 import logging
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import Any, Dict, Final, Type, TypeVar
+from typing import TYPE_CHECKING, Any, Dict, Final, Type, TypeVar
+
+from ..utils import hash_str
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from typing import Optional
 
 T = TypeVar('T', bound='Proof')
 
@@ -17,6 +24,11 @@ class ProofStatus(Enum):
 
 
 class Proof(ABC):
+    id: str
+
+    def __init__(self, id: str) -> None:
+        self.id = id
+
     @property
     @abstractmethod
     def status(self) -> ProofStatus:
@@ -31,3 +43,19 @@ class Proof(ABC):
     @abstractmethod
     def from_dict(cls: Type[Proof], dct: Dict[str, Any]) -> Proof:
         ...
+
+
+class Prover:
+    proof: Proof
+    proof_dir: Optional[Path]
+
+    def __init__(self, proof: Proof, proof_dir: Optional[Path] = None) -> None:
+        self.proof = proof
+        self.proof_dir = proof_dir
+
+    def write_proof(self) -> None:
+        if not self.proof_dir:
+            return
+        proof_path = self.proof_dir / f'{hash_str(self.proof.id)}.json'
+        proof_path.write_text(json.dumps(self.proof.dict))
+        _LOGGER.info(f'Updated proof file {self.proof.id}: {proof_path}')
