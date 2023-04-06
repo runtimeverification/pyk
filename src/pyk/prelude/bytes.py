64deec8322a08be441c2107a91430dbe245ee6d1
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-04-06 11:38:08 +0200

    Remove dequoting from `KToken`

--- src/pyk/prelude/bytes.py
+++ src/pyk/prelude/bytes.py
@@ -1,9 +1,18 @@
 from typing import Final
 
 from ..kast.inner import KSort, KToken
+from ..utils import dequote_str, enquote_str
 
 BYTES: Final = KSort('Bytes')
 
 
-def bytesToken(s: str) -> KToken:  # noqa: N802
-    return KToken('b"' + s + '"', BYTES)
+def bytesToken(pretty: str) -> KToken:  # noqa: N802
+    return KToken(f'b"{enquote_str(pretty)}"', BYTES)
+
+
+def pretty_bytes(token: KToken) -> str:
+    if token.sort != BYTES:
+        raise ValueError(f'Expected Bytes token, got: {token}')
+    assert token.token[0:2] == 'b"'
+    assert token.token[-1] == '"'
+    return dequote_str(token.token[2:-1])
--- src/pyk/prelude/string.py
+++ src/pyk/prelude/string.py
@@ -1,9 +1,17 @@
 from typing import Final
 
 from ..kast.inner import KSort, KToken
+from ..utils import dequote_str, enquote_str
 
 STRING: Final = KSort('String')
 
 
-def stringToken(s: str) -> KToken:  # noqa: N802
-    return KToken('"' + s + '"', STRING)
+def stringToken(pretty: str) -> KToken:  # noqa: N802
+    return KToken(f'"{enquote_str(pretty)}"', STRING)
+
+
+def pretty_string(token: KToken) -> str:
+    if token.sort != STRING:
+        raise ValueError(f'Expected String token, got: {token}')
+    assert token.token[0] == '"' == token.token[-1]
+    return dequote_str(token.token[1:-1])
