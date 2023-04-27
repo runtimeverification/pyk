34ec0c9e3f4eeaed8e92e26c190d9808bcd024e4
Georgy Lukyanov <mail@geo2a.info>
2023-04-27 13:16:36 +0200

    Implement proof execution

--- src/pyk/proof/schedule.py
+++ src/pyk/proof/schedule.py
@@ -2,23 +2,36 @@ from __future__ import annotations
 
 import json
 import logging
+import tempfile
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 import igraph
 
 from ..cli_utils import check_dir_path
 from ..utils import hash_str
-from .equality import EqualityProof
+from .equality import EqualityProof, EqualityProver
+from .proof import ProofStatus
 from .reachability import APRBMCProof, APRProof
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, Mapping
     from typing import Any, Final
 
+    from ..kcfg import KCFGExplore
     from .proof import Proof
 
 _LOGGER: Final = logging.getLogger(__name__)
+
+
+def proof_status_color(status: ProofStatus) -> str:
+    match status:
+        case ProofStatus.FAILED:
+            return 'red'
+        case ProofStatus.PENDING:
+            return 'yellow'
+        case ProofStatus.PASSED:
+            return 'green'
 
 
 class ProofSchedule:
@@ -51,6 +64,7 @@ class ProofSchedule:
         self.id = id
         # intialize the internal igraph.Graph datastructure with the user-suppleid list of edges
         self._proof_dag = igraph.Graph.TupleList(edges=proof_dag_edges, directed=True)
+
         self.proof_dir = proof_dir
         self._proofs = {}
 
@@ -64,17 +78,25 @@ class ProofSchedule:
             raise ValueError(f'Proof Schedule has a cycle from {loop_origin} to {loop_target}') from err
         # Load the actual proofs from disc
         for proof_id in self.proof_ids:
-            proof_file = proof_dir / f'{hash_str(proof_id)}.json'
-            proof_dict = json.loads(proof_file.read_text())
-            match proof_dict['type']:
-                case 'APRProof':
-                    self._proofs[proof_id] = APRProof.from_dict(proof_dict)
-                case 'APRBMCProof':
-                    self._proofs[proof_id] = APRBMCProof.from_dict(proof_dict)
-                case 'EqualityProof':
-                    self._proofs[proof_id] = EqualityProof.from_dict(proof_dict)
-                case other:
-                    raise ValueError(f'Unknown proof type {other} found in proof file {proof_file}')
+            self.load_proof(proof_id)
+
+        # initialize vertex colors according to their proof statuses (for visialization only)
+        for v in self._proof_dag.vs:
+            v['color'] = proof_status_color(self._proofs[v['name']].status)
+            v['label'] = v['name']
+
+    def load_proof(self, proof_id: str) -> None:
+        proof_file = self.proof_dir / f'{hash_str(proof_id)}.json'
+        proof_dict = json.loads(proof_file.read_text())
+        match proof_dict['type']:
+            case 'APRProof':
+                self._proofs[proof_id] = APRProof.from_dict(proof_dict)
+            case 'APRBMCProof':
+                self._proofs[proof_id] = APRBMCProof.from_dict(proof_dict)
+            case 'EqualityProof':
+                self._proofs[proof_id] = EqualityProof.from_dict(proof_dict)
+            case other:
+                raise ValueError(f'Unknown proof type {other} found in proof file {proof_file}')
 
     @property
     def topological_sorting(self) -> Iterable[Proof]:
@@ -133,9 +155,65 @@ class ProofSchedule:
             return ProofSchedule.from_dict(proof_dict, proof_dir=proof_dir)
         raise ValueError(f'Could not load ProofSchedule from file {id}: {proof_schedule_path}')
 
-    def write_svg(self, svg_path: Path | None = None) -> None:
-        svg_path = svg_path if svg_path else self.proof_dir / f'{hash_str(self.id)}.svg'
-        self._proof_dag.write_svg(fname=str(svg_path), labels='name')
+    @property
+    def dot(self) -> str:
+        # make sure the vertex status coloring is up-to-date
+        for v in self._proof_dag.vs:
+            v['color'] = proof_status_color(self._proofs[v['name']].status)
+        f = tempfile.TemporaryFile('r+')
+        self._proof_dag.write_dot(f)
+        f.seek(0)
+        result = f.read()
+        f.close()
+        return result
 
-    def advance_schedule(self) -> None:
-        pass
+    @property
+    def leafs(self) -> Iterable[Proof]:
+        """
+        Enumerate leaf proofs in the schedule, i.e. the nodes in the DAG that have out-degree of zero
+        """
+        return self._proof_dag.vs.select(_outdegree=0)['name']
+
+    @property
+    def roots(self) -> Iterable[Proof]:
+        return self._proof_dag.vs.select(_indegree=0)['name']
+
+    def direct_dependencies(self, proof_id: str) -> Iterable[str]:
+        """
+        Direct dependenices of a proof, i.e. the predicessors of the proof's node in the DAG
+        """
+        return [self._proof_dag.vs[v_id]['name'] for v_id in self._proof_dag.predecessors(proof_id)]
+
+    def _dependencies(self, proof_id: str) -> Iterable[str]:
+        """
+        ALL dependenices of a proof, i.e. the transitive predicessors of the proof's node in the DAG
+        """
+        deps_v_ids: set[int] = set()
+        for root in self.roots:
+            paths_to_root = self._proof_dag.get_shortest_paths(root, to=proof_id)
+            for path in paths_to_root:
+                for v_id in path:
+                    deps_v_ids.add(v_id)
+        return [self._proof_dag.vs[v_id]['name'] for v_id in deps_v_ids]
+
+    def fire_proof(self, proof_id: str, kcfg_explore: KCFGExplore) -> None:
+        """
+        Trigger a proof in the schedule, also triggiring its dependencies
+        """
+
+        deps_set = set(self._dependencies(proof_id))
+        # induce the subgraph generated by dependenices, including the target proof
+        deps_dag = self._proof_dag.induced_subgraph(deps_set)
+        proof_v = deps_dag.vs.find(proof_id)
+        # traverse the dependenices in reversed order and execute proofs
+        deps_dag.reverse_edges()
+        [vertices, _] = deps_dag.dfs(proof_v.index)
+        for v in vertices:
+            proof_name = deps_dag.vs[v]['name']
+            _LOGGER.info(f'Processing proof {proof_name}')
+            if self._proofs[proof_name].status == ProofStatus.PENDING:
+                prover = EqualityProver(proof=cast('EqualityProof', self._proofs[proof_name]))
+                prover.advance_proof(kcfg_explore)
+                # re-index the advanced proof into the schedule
+                self._proofs[proof_name] = prover.proof
+            _LOGGER.info(f'Proof {proof_name} {self._proofs[proof_name].status}')
--- src/tests/integration/test_proof_schedule.py
+++ src/tests/integration/test_proof_schedule.py
@@ -1,22 +1,24 @@
 from __future__ import annotations
 
 import itertools
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
 
-from pyk.prelude.kbool import BOOL
-from pyk.prelude.kint import intToken
 from pyk.proof.equality import EqualityProof
+from pyk.proof.proof import ProofStatus
 from pyk.proof.schedule import ProofSchedule
 
 from .utils import KCFGExploreTest
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
-    from pathlib import Path
 
+    # from pathlib import Path
     from pytest import TempPathFactory
+
+    from pyk.kcfg import KCFGExplore
 
 
 @pytest.fixture(scope='function')
@@ -25,29 +27,48 @@ def proof_dir(tmp_path_factory: TempPathFactory) -> Path:
 
 
 def equality_proofs(proof_dir: Path | None = None) -> Iterable[EqualityProof]:
-    for idx in itertools.count(start=1, step=1):
-        proof = EqualityProof(
-            id=f'equality_proof_{idx}',
-            lhs_body=intToken(idx),
-            rhs_body=intToken(idx),
-            sort=BOOL,
+    """
+    An infinite stream of one-simplification proofs like `3 + 4 = 7`
+    """
+    for idx in itertools.count(start=0, step=1):
+        proof = EqualityProof.from_dict(
             proof_dir=proof_dir,
+            dct={
+                'type': 'EqualityProof',
+                'id': f'equality_proof_{idx}',
+                'lhs_body': {
+                    'node': 'KApply',
+                    'label': {'node': 'KLabel', 'name': '_+Int_', 'params': []},
+                    'args': [
+                        {'node': 'KToken', 'token': str(idx), 'sort': {'node': 'KSort', 'name': 'Int'}},
+                        {'node': 'KToken', 'token': str(idx + 1), 'sort': {'node': 'KSort', 'name': 'Int'}},
+                    ],
+                    'arity': 2,
+                    'variable': False,
+                },
+                'rhs_body': {'node': 'KToken', 'token': str(idx + idx + 1), 'sort': {'node': 'KSort', 'name': 'Int'}},
+                'sort': {'node': 'KSort', 'name': 'Int'},
+                'lhs_constraints': [{'node': 'KToken', 'token': 'true', 'sort': {'node': 'KSort', 'name': 'Bool'}}],
+                'rhs_constraints': [{'node': 'KToken', 'token': 'true', 'sort': {'node': 'KSort', 'name': 'Bool'}}],
+            },
         )
         proof.write_proof()
         yield proof
 
 
 class TestProofSchedule(KCFGExploreTest):
+    KOMPILE_MAIN_FILE = 'k-files/imp-verification.k'
+
     TEST_DATA: Iterable[tuple[str, int, list[tuple[str, str]]]] = [
         (
             'init_linear',
             3,
-            [('equality_proof_1', 'equality_proof_2'), ('equality_proof_2', 'equality_proof_3')],
+            [('equality_proof_0', 'equality_proof_1'), ('equality_proof_1', 'equality_proof_2')],
         ),
         (
             'init_fork',
             3,
-            [('equality_proof_1', 'equality_proof_2'), ('equality_proof_1', 'equality_proof_3')],
+            [('equality_proof_0', 'equality_proof_1'), ('equality_proof_0', 'equality_proof_2')],
         ),
     ]
 
@@ -97,7 +118,7 @@ class TestProofSchedule(KCFGExploreTest):
         assert proof_schedule.dict == {
             'type': 'ProofSchedule',
             'id': 'test',
-            'proof_dag_edges': [('equality_proof_1', 'equality_proof_2'), ('equality_proof_2', 'equality_proof_3')],
+            'proof_dag_edges': [('equality_proof_0', 'equality_proof_1'), ('equality_proof_1', 'equality_proof_2')],
             'proof_dir': str(proof_dir),
         }
 
@@ -123,9 +144,9 @@ class TestProofSchedule(KCFGExploreTest):
 
         # Then
         assert [proof.id for proof in proof_schedule.topological_sorting] == [
+            'equality_proof_0',
             'equality_proof_1',
             'equality_proof_2',
-            'equality_proof_3',
         ]
 
     def test_topological_sorting_binary_tree(self, proof_dir: Path) -> None:
@@ -138,7 +159,99 @@ class TestProofSchedule(KCFGExploreTest):
 
         # Then
         assert [proof.id for proof in proof_schedule.topological_sorting] == [
+            'equality_proof_0',
             'equality_proof_1',
             'equality_proof_2',
-            'equality_proof_3',
         ]
+
+    def test_roots(self, proof_dir: Path) -> None:
+        proof1, proof2, proof3 = itertools.islice(equality_proofs(proof_dir=proof_dir), 3)
+        proof_dag_edges = [(proof1.id, proof2.id), (proof1.id, proof3.id)]
+
+        # When
+        proof_schedule = ProofSchedule(id='test', proof_dir=proof_dir, proof_dag_edges=proof_dag_edges)
+
+        # Then
+        assert proof_schedule.roots == [proof1.id]
+
+    def test_leafs(self, proof_dir: Path) -> None:
+        proof1, proof2, proof3 = itertools.islice(equality_proofs(proof_dir=proof_dir), 3)
+        proof_dag_edges = [(proof1.id, proof2.id), (proof1.id, proof3.id)]
+
+        # When
+        proof_schedule = ProofSchedule(id='test', proof_dir=proof_dir, proof_dag_edges=proof_dag_edges)
+
+        # Then
+        assert proof_schedule.leafs == [proof2.id, proof3.id]
+
+    def test_direct_dependencies(self, proof_dir: Path) -> None:
+        proof1, proof2, proof3 = itertools.islice(equality_proofs(proof_dir=proof_dir), 3)
+        proof_dag_edges = [(proof1.id, proof2.id), (proof1.id, proof3.id)]
+
+        # When
+        proof_schedule = ProofSchedule(id='test', proof_dir=proof_dir, proof_dag_edges=proof_dag_edges)
+
+        # Then
+        assert proof_schedule.direct_dependencies(proof2.id) == [proof1.id]
+        assert proof_schedule.direct_dependencies(proof3.id) == [proof1.id]
+
+    def test_dependencies(self, proof_dir: Path) -> None:
+        proofs = list(itertools.islice(equality_proofs(proof_dir=proof_dir), 5))
+        proof_dag_edges = [
+            (proofs[0].id, proofs[1].id),
+            (proofs[1].id, proofs[2].id),
+            (proofs[0].id, proofs[3].id),
+            (proofs[3].id, proofs[4].id),
+        ]
+
+        # When
+        proof_schedule = ProofSchedule(id='test', proof_dir=proof_dir, proof_dag_edges=proof_dag_edges)
+
+        # Then
+        assert set(proof_schedule._dependencies(proofs[2].id)) == {proofs[0].id, proofs[1].id, proofs[2].id}
+        assert set(proof_schedule._dependencies(proofs[4].id)) == {proofs[4].id, proofs[3].id, proofs[0].id}
+
+    def test_fire_proof(self, proof_dir: Path, kcfg_explore: KCFGExplore) -> None:
+        proofs = list(itertools.islice(equality_proofs(proof_dir=proof_dir), 5))
+        proof_dag_edges = [
+            (proofs[0].id, proofs[1].id),
+            (proofs[1].id, proofs[2].id),
+            (proofs[0].id, proofs[3].id),
+            (proofs[3].id, proofs[4].id),
+        ]
+
+        # When
+        proof_schedule = ProofSchedule(id='test', proof_dir=proof_dir, proof_dag_edges=proof_dag_edges)
+
+        proof_schedule.fire_proof(proofs[2].id, kcfg_explore)
+
+        for i in [0, 1, 2]:
+            assert proof_schedule._proofs[proofs[i].id].status == ProofStatus.PASSED
+        for i in [3, 4]:
+            assert proof_schedule._proofs[proofs[i].id].status == ProofStatus.PENDING
+
+    def test_fire_two_proofs(self, proof_dir: Path, kcfg_explore: KCFGExplore) -> None:
+        proofs = list(itertools.islice(equality_proofs(proof_dir=proof_dir), 5))
+        proof_dag_edges = [
+            (proofs[0].id, proofs[1].id),
+            (proofs[1].id, proofs[2].id),
+            (proofs[0].id, proofs[3].id),
+            (proofs[3].id, proofs[4].id),
+        ]
+
+        # When
+        proof_schedule = ProofSchedule(id='test', proof_dir=proof_dir, proof_dag_edges=proof_dag_edges)
+        proof_schedule.fire_proof(proofs[2].id, kcfg_explore)
+
+        # Then
+        for i in [0, 1, 2]:
+            assert proof_schedule._proofs[proofs[i].id].status == ProofStatus.PASSED
+        for i in [3, 4]:
+            assert proof_schedule._proofs[proofs[i].id].status == ProofStatus.PENDING
+
+        proof_schedule.fire_proof(proofs[4].id, kcfg_explore)
+        for i in [0, 1, 2, 3, 4]:
+            assert proof_schedule._proofs[proofs[i].id].status == ProofStatus.PASSED
+
+        f = Path('/home/geo2a/Desktop/t.dot')
+        f.write_text(proof_schedule.dot)
