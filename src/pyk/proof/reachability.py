1caf4a24db3649a1d189ede72b790dd30390068c
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2023-07-18 22:50:56 +0200

    Refactor implication checks

--- src/pyk/proof/reachability.py
+++ src/pyk/proof/reachability.py
@@ -489,8 +489,11 @@ class APRProver(Prover):
             iterations += 1
             curr_node = self.proof.pending[0]
 
-            if (self._is_terminal is None or self._check_terminal(curr_node)) or implication_every_block:
-                self._check_subsume(curr_node)
+            if implication_every_block or self._is_terminal is None or self._is_terminal(curr_node.cterm):
+                if self._check_subsume(curr_node):
+                    continue
+
+            if self._check_terminal(curr_node):
                 continue
 
             if self._check_abstract(curr_node):
