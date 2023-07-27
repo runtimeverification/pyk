cc922b7f95537427ae326fbaca192d9332041506
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-07-27 14:27:00 +0000

    profiling/profile_kast_json: adjustments from code review

--- src/tests/profiling/profile_kast_json.py
+++ src/tests/profiling/profile_kast_json.py
@@ -23,12 +23,13 @@ KAST_JSON_TEST_DATA: list[tuple[str, str]] = [
 @pytest.mark.parametrize('test_id,file_name', KAST_JSON_TEST_DATA, ids=[test_id for test_id, *_ in KAST_JSON_TEST_DATA])
 def test_kast_json(profile: Profiler, test_id: str, file_name: str) -> None:
     json_file = TEST_DATA_DIR / file_name
-    with profile('json-load.prof', sort_keys=('cumtime',), limit=20):
-        with json_file.open() as f:
-            json_data = json.load(f)
+    json_text = json_file.read_text()
 
-    with profile('json-to-kast.prof', sort_keys=('cumtime',), limit=20):
+    with profile('json-parse.prof', sort_keys=('cumtime',), limit=20):
+        json_data = json.loads(json_text)
+
+    with profile('json-to-kast.prof', sort_keys=('cumtime',), limit=35):
         kast: KAst = kast_term(json_data)
 
-    with profile('kast-to-json.prof', sort_keys=('cumtime',), limit=20):
+    with profile('kast-to-json.prof', sort_keys=('cumtime',), limit=35):
         kast.to_dict()
