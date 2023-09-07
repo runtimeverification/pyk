5fa8c8ba846fc1329d02c15f5cd5570e1f737a39
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-09-07 17:19:48 +0200

    Collapse `Package` class hierarchy

--- src/pyk/kbuild/package.py
+++ src/pyk/kbuild/package.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
@@ -9,26 +8,23 @@ from typing import final
 from .project import Project
 
 
-class Package(ABC):
+@final
+@dataclass(frozen=True)
+class Package:
+    project: Project
+
     @staticmethod
     def create(project_file: str | Path) -> Package:
         project = Project.load(project_file)
-        return _RootPackage(project)
+        return Package(project)
 
     @property
-    @abstractmethod
     def name(self) -> str:
-        ...
+        return self.project.name
 
     @property
-    @abstractmethod
     def source(self) -> Path:
-        ...
-
-    @property
-    @abstractmethod
-    def project(self) -> Project:
-        ...
+        return self.project.path
 
     @property
     def path(self) -> Path:
@@ -48,7 +44,7 @@ class Package(ABC):
 
     @cached_property
     def deps_packages(self) -> tuple[Package, ...]:
-        return tuple(_RootPackage(project) for project in self.project.dependencies)
+        return tuple(Package(project) for project in self.project.dependencies)
 
     @cached_property
     def sub_packages(self) -> tuple[Package, ...]:
@@ -56,21 +52,3 @@ class Package(ABC):
         for package in self.deps_packages:
             res += package.sub_packages
         return res
-
-
-@final
-@dataclass(frozen=True)
-class _RootPackage(Package):
-    _project: Project
-
-    @property
-    def name(self) -> str:
-        return self.project.name
-
-    @property
-    def source(self) -> Path:
-        return self.project.path
-
-    @property
-    def project(self) -> Project:
-        return self._project
