a60290c24487dc63a14caf598f55fed20e5a44c2
Noah Watson <noah@nwatson.xyz>
2023-11-06 13:20:11 -0600

    Change interface to use ProofStep

--- src/pyk/proof/parallel.py
+++ src/pyk/proof/parallel.py
@@ -1,20 +1,15 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any, Generic, TypeVar
-
-from pyk.proof.reachability import APRProof
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
-    from pyk.kcfg.kcfg import CTerm
     from pyk.proof.proof import ProofStatus
-    from pyk.proof.reachability import APRProver
 
-P = TypeVar('P', bound='Any')
-S = TypeVar('S', bound='Any')
+P = TypeVar('P', bound='Proof')
+S = TypeVar('S', bound='ProofStep')
 U = TypeVar('U', bound='Any')
 
 
@@ -23,69 +18,70 @@ class Prover(ABC, Generic[P, S, U]):
     def steps(self, proof: P) -> Iterable[S]:
         ...
 
-    @classmethod
-    @abstractmethod
-    def advance(cls, step: S) -> U:
-        ...
-
     @abstractmethod
     def commit(self, proof: P, update: U) -> None:
         ...
 
 
-class Proof:
+class Proof(ABC):
     @property
     @abstractmethod
     def status(self) -> ProofStatus:
         ...
 
 
-@dataclass
-class APRProverTask:
-    cterm: CTerm
-    node_id: int
-    module_name: str
-    execute_depth: int
-    cut_point_rules: Iterable[str]
-    terminal_rules: Iterable[str]
-
-
-class APRProverResult:
-    ...
-
-
-class NewAPRProver(Prover[APRProof, APRProverTask, APRProverResult]):
-    prover: APRProver
-    execute_depth: int
-    cut_point_rules: Iterable[str]
-    terminal_rules: Iterable[str]
-
-    def __init__(self, prover: APRProver) -> None:
-        self.prover = prover
-
-    def steps(self, proof: APRProof) -> Iterable[APRProverTask]:
-        steps = []
-        for node in proof.pending:
-            module_name = (
-                self.prover.circularities_module_name
-                if self.prover.nonzero_depth(node)
-                else self.prover.dependencies_module_name
-            )
-            steps.append(
-                APRProverTask(
-                    cterm=node.cterm,
-                    node_id=node.id,
-                    module_name=module_name,
-                    execute_depth=self.execute_depth,
-                    cut_point_rules=self.cut_point_rules,
-                    terminal_rules=self.terminal_rules,
-                )
-            )
-        return steps
-
-    @classmethod
-    def advance(cls, step: APRProverTask) -> APRProverResult:
-        return APRProverResult()
-
-    def commit(self, proof: APRProof, update: APRProverResult) -> None:
+class ProofStep(ABC, Generic[U]):
+    @abstractmethod
+    def exec(self) -> U:
         ...
+
+
+#  @dataclass
+#  class APRProverTask:
+#      cterm: CTerm
+#      node_id: int
+#      module_name: str
+#      execute_depth: int
+#      cut_point_rules: Iterable[str]
+#      terminal_rules: Iterable[str]
+#
+#
+#  class APRProverResult:
+#      ...
+#
+#
+#  class NewAPRProver(Prover[APRProof, APRProverTask, APRProverResult]):
+#      prover: APRProver
+#      execute_depth: int
+#      cut_point_rules: Iterable[str]
+#      terminal_rules: Iterable[str]
+#
+#      def __init__(self, prover: APRProver) -> None:
+#          self.prover = prover
+#
+#      def steps(self, proof: APRProof) -> Iterable[APRProverTask]:
+#          steps = []
+#          for node in proof.pending:
+#              module_name = (
+#                  self.prover.circularities_module_name
+#                  if self.prover.nonzero_depth(node)
+#                  else self.prover.dependencies_module_name
+#              )
+#              steps.append(
+#                  APRProverTask(
+#                      cterm=node.cterm,
+#                      node_id=node.id,
+#                      module_name=module_name,
+#                      execute_depth=self.execute_depth,
+#                      cut_point_rules=self.cut_point_rules,
+#                      terminal_rules=self.terminal_rules,
+#                  )
+#              )
+#          return steps
+#
+#      @classmethod
+#      def advance(cls, step: APRProverTask) -> APRProverResult:
+#          return APRProverResult()
+#
+#      def commit(self, proof: APRProof, update: APRProverResult) -> None:
+#          ...
--- src/tests/integration/proof/test_parallel_prove.py
+++ src/tests/integration/proof/test_parallel_prove.py
@@ -3,27 +3,15 @@ from __future__ import annotations
 import sys
 import time
 from concurrent.futures import ProcessPoolExecutor, wait
-from pathlib import Path
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-import pytest
-
-from pyk.proof.parallel import APRProof, APRProver, NewAPRProver, Proof, Prover
+from pyk.proof.parallel import Proof, ProofStep, Prover
 from pyk.proof.proof import ProofStatus
-from pyk.testing import KCFGExploreTest, KProveTest
-from pyk.utils import single
-
-from ..utils import K_FILES
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
     from concurrent.futures import Executor, Future
-
-    from pytest import TempPathFactory
-
-    from pyk.kcfg.explore import KCFGExplore
-    from pyk.ktool.kprove import KProve
-    from pyk.proof.parallel import APRProverResult, APRProverTask
 
 
 class TreeExploreProof(Proof):
@@ -66,48 +54,55 @@ class TreeExploreProof(Proof):
             return ProofStatus.PENDING
 
 
-class TreeExploreProver(Prover[TreeExploreProof, int, int]):
+@dataclass
+class TreeExploreProofStep(ProofStep):
+    node: int
+
+    def __hash__(self) -> int:
+        return self.node.__hash__()
+
+    def exec(self) -> int:
+        print(f'Advancing node {self.node}\n', file=sys.stderr)
+        time.sleep(5)
+        print(f'Done advancing node {self.node}\n', file=sys.stderr)
+        return self.node
+
+
+class TreeExploreProver(Prover[TreeExploreProof, TreeExploreProofStep, int]):
     def __init__(self) -> None:
         return
 
-    def steps(self, proof: TreeExploreProof) -> Iterable[int]:
+    def steps(self, proof: TreeExploreProof) -> Iterable[TreeExploreProofStep]:
         def parents(node_id: int) -> Iterable[int]:
             return [source for source, targets in proof.edges.items() if node_id in targets]
 
         nodes = set(range(10))
 
         return [
-            node_id
+            TreeExploreProofStep(node_id)
             for node_id in nodes
             if node_id not in proof.reached and all(parent in proof.reached for parent in parents(node_id))
         ]
-
-    @classmethod
-    def advance(cls, step: int) -> int:
-        print(f'Advancing node {step}\n', file=sys.stderr)
-        time.sleep(5)
-        print(f'Done advancing node {step}\n', file=sys.stderr)
-        return step
 
     def commit(self, proof: TreeExploreProof, update: int) -> None:
         proof.reached.add(update)
 
 
 def prove_parallel(
-    proofs: list[APRProof],
+    proofs: list[TreeExploreProof],
     # We need a way to map proofs to provers, but for simplicity, I'll assume it as a given
-    provers: dict[APRProof, NewAPRProver],
-) -> Iterable[APRProof]:
-    pending: dict[Future[APRProverResult], APRProof] = {}
-    explored: set[APRProverTask] = set()
+    provers: dict[TreeExploreProof, TreeExploreProver],
+) -> Iterable[TreeExploreProof]:
+    pending: dict[Future[int], TreeExploreProof] = {}
+    explored: set[TreeExploreProofStep] = set()
 
-    def submit(proof: APRProof, pool: Executor) -> None:
+    def submit(proof: TreeExploreProof, pool: Executor) -> None:
         prover = provers[proof]
         for step in prover.steps(proof):  # <-- get next steps (represented by e.g. pending nodes, ...)
             if step in explored:
                 continue
             explored.add(step)
-            future = pool.submit(prover.advance, step)  # <-- schedule steps for execution
+            future = pool.submit(step.exec)  # <-- schedule steps for execution
             pending[future] = proof
 
     with ProcessPoolExecutor(max_workers=2) as pool:
@@ -135,26 +130,34 @@ def prove_parallel(
     return proofs
 
 
-@pytest.fixture(scope='function')
-def proof_dir(tmp_path_factory: TempPathFactory) -> Path:
-    return tmp_path_factory.mktemp('proofs')
+def test_parallel_prove() -> None:
+    prover = TreeExploreProver()
+    proof = TreeExploreProof()
+    results = prove_parallel([proof], {proof: prover})
+    assert len(list(results)) == 1
+    assert list(results)[0].status == ProofStatus.PASSED
 
 
-class TestAPRProofParallel(KCFGExploreTest, KProveTest):
-    KOMPILE_MAIN_FILE = K_FILES / 'imp.k'
-
-    def test_parallel_prove(self, kprove: KProve, kcfg_explore: KCFGExplore) -> None:
-        spec_file = K_FILES / 'imp-simple-spec.k'
-        spec_module = 'IMP-SPEC'
-        claim_id = 'concrete-addition'
-
-        claim = single(
-            kprove.get_claims(Path(spec_file), spec_module_name=spec_module, claim_labels=[f'{spec_module}.{claim_id}'])
-        )
-
-        proof = APRProof.from_claim(defn=kprove.definition, claim=claim, logs={})
-        prover = APRProver(proof=proof, kcfg_explore=kcfg_explore)
-        new_prover = NewAPRProver(prover=prover)
-        results = prove_parallel([proof], {proof: new_prover})
-        assert len(list(results)) == 1
-        assert list(results)[0].status == ProofStatus.PASSED
+#  @pytest.fixture(scope='function')
+#  def proof_dir(tmp_path_factory: TempPathFactory) -> Path:
+#      return tmp_path_factory.mktemp('proofs')
+#
+#
+#  class TestAPRProofParallel(KCFGExploreTest, KProveTest):
+#      KOMPILE_MAIN_FILE = K_FILES / 'imp.k'
+#
+#      def test_parallel_prove(self, kprove: KProve, kcfg_explore: KCFGExplore) -> None:
+#          spec_file = K_FILES / 'imp-simple-spec.k'
+#          spec_module = 'IMP-SPEC'
+#          claim_id = 'concrete-addition'
+#
+#          claim = single(
+#              kprove.get_claims(Path(spec_file), spec_module_name=spec_module, claim_labels=[f'{spec_module}.{claim_id}'])
+#          )
+#
+#          proof = APRProof.from_claim(defn=kprove.definition, claim=claim, logs={})
+#          prover = APRProver(proof=proof, kcfg_explore=kcfg_explore)
+#          new_prover = NewAPRProver(prover=prover)
+#          results = prove_parallel([proof], {proof: new_prover})
+#          assert len(list(results)) == 1
+#          assert list(results)[0].status == ProofStatus.PASSED
