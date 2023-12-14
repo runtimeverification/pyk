de3df13a4e156db631b48e26fa84dde19435309c
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-12-14 20:33:15 +0000

    Do not write to disk

--- src/pyk/kore/tools.py
+++ src/pyk/kore/tools.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 from enum import Enum
 from pathlib import Path
-from tempfile import NamedTemporaryFile
+from typing import TYPE_CHECKING
 
 from ..cli.utils import check_dir_path, check_file_path
 from ..utils import run_process
-from .syntax import Pattern
+
+if TYPE_CHECKING:
+    from .syntax import Pattern
 
 
 class PrintOutput(Enum):
@@ -30,18 +32,15 @@ def kore_print(
 ) -> str:
     if output is PrintOutput.KORE and isinstance(pattern, str):
         return pattern
-
-    with NamedTemporaryFile(mode='w') as f:
-        if isinstance(pattern, Pattern):
-            pattern.write(f)
-            f.write('\n')
-        elif isinstance(pattern, str):
-            f.write(pattern)
-        else:
-            raise TypeError(f'Unexpected type, expected [str | Pattern], got: {type(pattern)}')
-        f.flush()
-
-        return _kore_print(f.name, definition_dir=definition_dir, output_file=output_file, output=output, color=color)
+    input = pattern if isinstance(pattern, str) else pattern.text
+    return _kore_print(
+        '/dev/stdin',
+        definition_dir=definition_dir,
+        output_file=output_file,
+        output=output,
+        color=color,
+        input=input,
+    )
 
 
 def _kore_print(
@@ -51,11 +50,14 @@ def _kore_print(
     output_file: str | Path | None = None,
     output: str | PrintOutput | None = None,
     color: bool | None = None,
+    # ---
+    input: str | None,
 ) -> str:
     args = ['kore-print']
 
     input_file = Path(input_file)
-    check_file_path(input_file)
+    if not input_file.is_char_device():
+        check_file_path(input_file)
     args += [str(input_file)]
 
     if definition_dir is not None:
@@ -75,5 +77,5 @@ def _kore_print(
     if color is not None:
         args += ['--color', 'on' if color else 'off']
 
-    run_res = run_process(args)
+    run_res = run_process(args, input=input)
     return run_res.stdout.strip()
