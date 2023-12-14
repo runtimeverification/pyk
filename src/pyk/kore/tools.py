66a7e0036c36a63a0ed3c27759469071e384535c
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-12-14 19:30:58 +0000

    Extract function `_kore_print`

--- src/pyk/kore/tools.py
+++ src/pyk/kore/tools.py
@@ -4,7 +4,7 @@ from enum import Enum
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
-from ..cli.utils import check_dir_path
+from ..cli.utils import check_dir_path, check_file_path
 from ..utils import run_process
 from .syntax import Pattern
 
@@ -20,13 +20,15 @@ class PrintOutput(Enum):
     NONE = 'none'
 
 
-def kore_print(pattern: str | Pattern, definition_dir: str | Path, output: str | PrintOutput) -> str:
-    definition_dir = Path(definition_dir)
-    check_dir_path(definition_dir)
+def kore_print(
+    pattern: str | Pattern,
+    definition_dir: str | Path | None = None,
+    output: str | PrintOutput | None = None,
+) -> str:
+    if output is not None:
+        output = PrintOutput(output)
 
-    output = PrintOutput(output)
-
-    if type(pattern) is str and output == PrintOutput.KORE:
+    if type(pattern) is str and output is PrintOutput.KORE:
         return pattern
 
     with NamedTemporaryFile(mode='w') as f:
@@ -39,6 +41,28 @@ def kore_print(pattern: str | Pattern, definition_dir: str | Path, output: str |
             raise TypeError(f'Unexpected type, expected [str | Pattern], got: {type(pattern)}')
         f.flush()
 
-        run_res = run_process(['kore-print', f.name, '--definition', str(definition_dir), '--output', output.value])
+        return _kore_print(f.name, definition_dir, output)
 
+
+def _kore_print(
+    input_file: str | Path,
+    definition_dir: str | Path | None = None,
+    output: str | PrintOutput | None = None,
+) -> str:
+    args = ['kore-print']
+
+    input_file = Path(input_file)
+    check_file_path(input_file)
+    args += [str(input_file)]
+
+    if definition_dir is not None:
+        definition_dir = Path(definition_dir)
+        check_dir_path(definition_dir)
+        args += ['--definition', str(definition_dir)]
+
+    if output is not None:
+        output = PrintOutput(output)
+        args += ['--output', output.value]
+
+    run_res = run_process(args)
     return run_res.stdout.strip()
