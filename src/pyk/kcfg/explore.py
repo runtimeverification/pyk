706044751bdd846594e9eec175295d787b354c24
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-01-16 06:34:31 +0000

    kcfg/{kcfg,explore}: attach rewrite steps to KCFG edges

--- src/pyk/kcfg/explore.py
+++ src/pyk/kcfg/explore.py
@@ -20,7 +20,7 @@ from ..kast.manip import (
 )
 from ..kast.outer import KRule
 from ..konvert import krule_to_kore
-from ..kore.rpc import AbortedResult, SatResult, StopReason, UnknownResult, UnsatResult
+from ..kore.rpc import AbortedResult, RewriteSuccess, SatResult, StopReason, UnknownResult, UnsatResult
 from ..kore.syntax import Import, Module
 from ..prelude import k
 from ..prelude.k import GENERATED_TOP_CELL
@@ -493,7 +493,8 @@ class KCFGExplore:
             case Step(cterm, depth, next_node_logs, cut):
                 next_node = kcfg.create_node(cterm)
                 logs[next_node.id] = next_node_logs
-                kcfg.create_edge(node.id, next_node.id, depth)
+                rules = [log.result.rule_id for log in next_node_logs if type(log.result) is RewriteSuccess]
+                kcfg.create_edge(node.id, next_node.id, depth, rules)
                 cut_str = 'cut-rule ' if cut else ''
                 log(f'{cut_str}basic block at depth {depth}: {node.id} -> {next_node.id}')
 
