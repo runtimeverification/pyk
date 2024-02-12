aacfb522af1dd2e69293f5f402b15c0787b5f092
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-02-12 15:22:04 +0000

    Extract module `_module_to_kore`

--- src/pyk/konvert/__init__.py
+++ src/pyk/konvert/__init__.py
@@ -1,10 +1,3 @@
-from ._konvert import (
-    _kast_to_kore,
-    _kore_to_kast,
-    kast_to_kore,
-    kflatmodule_to_kore,
-    kore_to_kast,
-    krule_to_kore,
-    module_to_kore,
-)
+from ._konvert import _kast_to_kore, _kore_to_kast, kast_to_kore, kflatmodule_to_kore, kore_to_kast, krule_to_kore
+from ._module_to_kore import module_to_kore
 from ._utils import munge, unmunge
