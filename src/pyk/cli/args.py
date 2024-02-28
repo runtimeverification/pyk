3103879271aa70802f5688832014dd509fa6d372
Noah Watson <noah@nwatson.xyz>
2024-02-28 13:50:53 -0600

    Mmove kompile_args to KompileOptions

--- src/pyk/cli/args.py
+++ src/pyk/cli/args.py
@@ -287,7 +287,7 @@ class ProveOptions(DefinitionOptions, OutputFileOptions):
         return base
 
 
-class KDefinitionOptions:
+class KDefinitionOptions(Options):
     @staticmethod
     def args(parser: ArgumentParser) -> ArgumentParser:
         parser.add_argument(
@@ -305,7 +305,7 @@ class KDefinitionOptions:
         return parser
 
 
-class SpecOptions:
+class SpecOptions(Options):
     @staticmethod
     def args(parser: ArgumentParser) -> ArgumentParser:
         parser.add_argument('spec_file', type=file_path, help='Path to spec file.')
@@ -324,6 +324,61 @@ class SpecOptions:
             action='append',
             help='Skip listed claims, MODULE_NAME.claim-id',
         )
+        return parser
+
+
+class KompileOptions(Options):
+    @staticmethod
+    def args(parser: ArgumentParser) -> ArgumentParser:
+        parser.add_argument(
+            '--emit-json',
+            dest='emit_json',
+            default=True,
+            action='store_true',
+            help='Emit JSON definition after compilation.',
+        )
+        parser.add_argument(
+            '--no-emit-json', dest='emit_json', action='store_false', help='Do not JSON definition after compilation.'
+        )
+        parser.add_argument(
+            '-ccopt',
+            dest='ccopts',
+            default=[],
+            action='append',
+            help='Additional arguments to pass to llvm-kompile.',
+        )
+        parser.add_argument(
+            '--no-llvm-kompile',
+            dest='llvm_kompile',
+            default=True,
+            action='store_false',
+            help='Do not run llvm-kompile process.',
+        )
+        parser.add_argument(
+            '--with-llvm-library',
+            dest='llvm_library',
+            default=False,
+            action='store_true',
+            help='Make kompile generate a dynamic llvm library.',
+        )
+        parser.add_argument(
+            '--enable-llvm-debug',
+            dest='enable_llvm_debug',
+            default=False,
+            action='store_true',
+            help='Make kompile generate debug symbols for llvm.',
+        )
+        parser.add_argument(
+            '--read-only-kompiled-directory',
+            dest='read_only',
+            default=False,
+            action='store_true',
+            help='Generated a kompiled directory that K will not attempt to write to afterwards.',
+        )
+        parser.add_argument('-O0', dest='o0', default=False, action='store_true', help='Optimization level 0.')
+        parser.add_argument('-O1', dest='o1', default=False, action='store_true', help='Optimization level 1.')
+        parser.add_argument('-O2', dest='o2', default=False, action='store_true', help='Optimization level 2.')
+        parser.add_argument('-O3', dest='o3', default=False, action='store_true', help='Optimization level 3.')
         return parser
 
 
@@ -349,60 +404,6 @@ class KCLIArgs:
             type=bug_report_arg,
             help='Generate bug report with given name',
         )
-        return args
-
-    @cached_property
-    def kompile_args(self) -> ArgumentParser:
-        args = ArgumentParser(add_help=False)
-        args.add_argument(
-            '--emit-json',
-            dest='emit_json',
-            default=True,
-            action='store_true',
-            help='Emit JSON definition after compilation.',
-        )
-        args.add_argument(
-            '--no-emit-json', dest='emit_json', action='store_false', help='Do not JSON definition after compilation.'
-        )
-        args.add_argument(
-            '-ccopt',
-            dest='ccopts',
-            default=[],
-            action='append',
-            help='Additional arguments to pass to llvm-kompile.',
-        )
-        args.add_argument(
-            '--no-llvm-kompile',
-            dest='llvm_kompile',
-            default=True,
-            action='store_false',
-            help='Do not run llvm-kompile process.',
-        )
-        args.add_argument(
-            '--with-llvm-library',
-            dest='llvm_library',
-            default=False,
-            action='store_true',
-            help='Make kompile generate a dynamic llvm library.',
-        )
-        args.add_argument(
-            '--enable-llvm-debug',
-            dest='enable_llvm_debug',
-            default=False,
-            action='store_true',
-            help='Make kompile generate debug symbols for llvm.',
-        )
-        args.add_argument(
-            '--read-only-kompiled-directory',
-            dest='read_only',
-            default=False,
-            action='store_true',
-            help='Generated a kompiled directory that K will not attempt to write to afterwards.',
-        )
-        args.add_argument('-O0', dest='o0', default=False, action='store_true', help='Optimization level 0.')
-        args.add_argument('-O1', dest='o1', default=False, action='store_true', help='Optimization level 1.')
-        args.add_argument('-O2', dest='o2', default=False, action='store_true', help='Optimization level 2.')
-        args.add_argument('-O3', dest='o3', default=False, action='store_true', help='Optimization level 3.')
         return args
 
     @cached_property
