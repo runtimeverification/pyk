b3693d32a458fd5df8b1eedf6937542b67b8cae1
Noah Watson <noah@nwatson.xyz>
2024-03-12 15:50:14 -0500

    Split Options hierarchy off from larger PR

--- src/pyk/cli/args.py
+++ src/pyk/cli/args.py
@@ -1,30 +1,172 @@
 from __future__ import annotations
 
+import sys
 from argparse import ArgumentParser
 from functools import cached_property
-from typing import TYPE_CHECKING
+from typing import IO, TYPE_CHECKING, Any
 
-from ..utils import ensure_dir_path
+from pyk.utils import ensure_dir_path
+
+from .cli import Options
 from .utils import bug_report_arg, dir_path, file_path
 
 if TYPE_CHECKING:
-    from typing import TypeVar
+    from pathlib import Path
 
-    T = TypeVar('T')
+    from ..utils import BugReport
+
+
+class LoggingOptions(Options):
+    debug: bool
+    verbose: bool
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'verbose': False,
+            'debug': False,
+        }
+
+
+class OutputFileOptions(Options):
+    output_file: IO[Any]
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'output_file': sys.stdout,
+        }
+
+
+class DefinitionOptions(Options):
+    definition_dir: Path
+
+
+class DisplayOptions(Options):
+    minimize: bool
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'minimize': True,
+        }
+
+
+class KDefinitionOptions(Options):
+    includes: list[str]
+    main_module: str | None
+    syntax_module: str | None
+    spec_module: str | None
+    definition_dir: Path | None
+    md_selector: str
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'spec_module': None,
+            'main_module': None,
+            'syntax_module': None,
+            'definition_dir': None,
+            'md_selector': 'k',
+            'includes': [],
+        }
+
+
+class SaveDirOptions(Options):
+    save_directory: Path | None
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'save_directory': None,
+        }
+
+
+class SpecOptions(SaveDirOptions):
+    spec_file: Path
+    claim_labels: list[str] | None
+    exclude_claim_labels: list[str]
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'claim_labels': None,
+            'exclude_claim_labels': [],
+        }
+
+
+class KompileOptions(Options):
+    emit_json: bool
+    ccopts: list[str]
+    llvm_kompile: bool
+    llvm_library: bool
+    enable_llvm_debug: bool
+    read_only: bool
+    o0: bool
+    o1: bool
+    o2: bool
+    o3: bool
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'emit_json': True,
+            'llvm_kompile': False,
+            'llvm_library': False,
+            'enable_llvm_debug': False,
+            'read_only': False,
+            'o0': False,
+            'o1': False,
+            'o2': False,
+            'o3': False,
+            'ccopts': [],
+        }
+
+
+class ParallelOptions(Options):
+    workers: int
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'workers': 1,
+        }
+
+
+class BugReportOptions(Options):
+    bug_report: BugReport | None
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {'bug_report': None}
+
+
+class SMTOptions(Options):
+    smt_timeout: int
+    smt_retry_limit: int
+    smt_tactic: str | None
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        return {
+            'smt_timeout': 300,
+            'smt_retry_limit': 10,
+            'smt_tactic': None,
+        }
 
 
 class KCLIArgs:
     @cached_property
     def logging_args(self) -> ArgumentParser:
         args = ArgumentParser(add_help=False)
-        args.add_argument('--verbose', '-v', default=False, action='store_true', help='Verbose output.')
-        args.add_argument('--debug', default=False, action='store_true', help='Debug output.')
+        args.add_argument('--verbose', '-v', default=None, action='store_true', help='Verbose output.')
+        args.add_argument('--debug', default=None, action='store_true', help='Debug output.')
         return args
 
     @cached_property
     def parallel_args(self) -> ArgumentParser:
         args = ArgumentParser(add_help=False)
-        args.add_argument('--workers', '-j', default=1, type=int, help='Number of processes to run in parallel.')
+        args.add_argument('--workers', '-j', type=int, help='Number of processes to run in parallel.')
         return args
 
     @cached_property
@@ -43,7 +185,7 @@ class KCLIArgs:
         args.add_argument(
             '--emit-json',
             dest='emit_json',
-            default=True,
+            default=None,
             action='store_true',
             help='Emit JSON definition after compilation.',
         )
@@ -53,42 +195,41 @@ class KCLIArgs:
         args.add_argument(
             '-ccopt',
             dest='ccopts',
-            default=[],
             action='append',
             help='Additional arguments to pass to llvm-kompile.',
         )
         args.add_argument(
             '--no-llvm-kompile',
             dest='llvm_kompile',
-            default=True,
+            default=None,
             action='store_false',
             help='Do not run llvm-kompile process.',
         )
         args.add_argument(
             '--with-llvm-library',
             dest='llvm_library',
-            default=False,
+            default=None,
             action='store_true',
             help='Make kompile generate a dynamic llvm library.',
         )
         args.add_argument(
             '--enable-llvm-debug',
             dest='enable_llvm_debug',
-            default=False,
+            default=None,
             action='store_true',
             help='Make kompile generate debug symbols for llvm.',
         )
         args.add_argument(
             '--read-only-kompiled-directory',
             dest='read_only',
-            default=False,
+            default=None,
             action='store_true',
             help='Generated a kompiled directory that K will not attempt to write to afterwards.',
         )
-        args.add_argument('-O0', dest='o0', default=False, action='store_true', help='Optimization level 0.')
-        args.add_argument('-O1', dest='o1', default=False, action='store_true', help='Optimization level 1.')
-        args.add_argument('-O2', dest='o2', default=False, action='store_true', help='Optimization level 2.')
-        args.add_argument('-O3', dest='o3', default=False, action='store_true', help='Optimization level 3.')
+        args.add_argument('-O0', dest='o0', default=None, action='store_true', help='Optimization level 0.')
+        args.add_argument('-O1', dest='o1', default=None, action='store_true', help='Optimization level 1.')
+        args.add_argument('-O2', dest='o2', default=None, action='store_true', help='Optimization level 2.')
+        args.add_argument('-O3', dest='o3', default=None, action='store_true', help='Optimization level 3.')
         return args
 
     @cached_property
@@ -112,7 +253,7 @@ class KCLIArgs:
     @cached_property
     def display_args(self) -> ArgumentParser:
         args = ArgumentParser(add_help=False)
-        args.add_argument('--minimize', dest='minimize', default=True, action='store_true', help='Minimize output.')
+        args.add_argument('--minimize', dest='minimize', default=None, action='store_true', help='Minimize output.')
         args.add_argument('--no-minimize', dest='minimize', action='store_false', help='Do not minimize output.')
         return args
 
@@ -120,10 +261,10 @@ class KCLIArgs:
     def definition_args(self) -> ArgumentParser:
         args = ArgumentParser(add_help=False)
         args.add_argument(
-            '-I', type=str, dest='includes', default=[], action='append', help='Directories to lookup K definitions in.'
+            '-I', type=str, dest='includes', action='append', help='Directories to lookup K definitions in.'
         )
-        args.add_argument('--main-module', default=None, type=str, help='Name of the main module.')
-        args.add_argument('--syntax-module', default=None, type=str, help='Name of the syntax module.')
+        args.add_argument('--main-module', type=str, help='Name of the main module.')
+        args.add_argument('--syntax-module', type=str, help='Name of the syntax module.')
         args.add_argument('--definition', type=dir_path, dest='definition_dir', help='Path to definition to use.')
         args.add_argument(
             '--md-selector',
