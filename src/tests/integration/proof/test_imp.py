76bee48aeea19d889c95fca57c8460ddd26440b7
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-03-13 15:00:20 +0000

    tests/proof/test_imp: remove unused test data

--- src/tests/integration/proof/test_imp.py
+++ src/tests/integration/proof/test_imp.py
@@ -86,17 +86,6 @@ class ImpSemantics(KCFGSemantics):
         return False
 
 
-PROVE_CTERM_TEST_DATA: Final = (
-    ('step-1', ['--depth', '1'], 'int $n , $s ; $n = 3 ;', [('int $s , .Ids ; $n = 3 ;', '$n |-> 0')]),
-    ('step-2', ['--depth', '2'], 'int $n , $s ; $n = 3 ;', [('int .Ids ; $n = 3 ;', '$n |-> 0 $s |-> 0')]),
-    (
-        'branch',
-        ['--max-counterexamples', '2', '--depth', '4'],
-        'int $n ; if (_B:Bool) { $n = 1; } else { $n = 2; }',
-        [('$n = 1 ;', '$n |-> 0'), ('$n = 2 ;', '$n |-> 0')],
-    ),
-)
-
 EMPTY_STATES: Final[list[tuple[str, str]]] = []
 EXECUTE_TEST_DATA: Final = (
     (
