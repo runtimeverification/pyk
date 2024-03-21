433d2282b086d49934a371980dc359add57b1e51
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-03-21 15:29:13 +0000

    ktool/kompile: support --no-exc-wrap option in Kompile calls

--- src/pyk/ktool/kompile.py
+++ src/pyk/ktool/kompile.py
@@ -353,6 +353,7 @@ class KompileArgs:
     bison_lists: bool
     warnings: Warnings | None
     warning_to_error: bool
+    no_exc_wrap: bool
 
     def __init__(
         self,
@@ -373,6 +374,7 @@ class KompileArgs:
         bison_lists: bool = False,
         warnings: str | Warnings | None = None,
         warning_to_error: bool = False,
+        no_exc_wrap: bool = False,
     ):
         main_file = Path(main_file)
         include_dirs = tuple(sorted(Path(include_dir) for include_dir in include_dirs))
@@ -395,6 +397,7 @@ class KompileArgs:
         object.__setattr__(self, 'bison_lists', bison_lists)
         object.__setattr__(self, 'warnings', warnings)
         object.__setattr__(self, 'warning_to_error', warning_to_error)
+        object.__setattr__(self, 'no_exc_wrap', no_exc_wrap)
 
     def args(self) -> list[str]:
         args = [str(self.main_file)]
@@ -443,6 +446,9 @@ class KompileArgs:
 
         if self.warning_to_error:
             args += ['-w2e']
+
+        if self.no_exc_wrap:
+            args += ['--no-exc-wrap']
 
         return args
 
