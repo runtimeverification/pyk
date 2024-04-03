ca02a8beaad4b96f280d57ffb5f35f8a5eb7a270
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2024-04-03 14:55:24 -0300

    Adding unit test and fixing requires call

--- src/pyk/kllvm/utils.py
+++ src/pyk/kllvm/utils.py
@@ -10,5 +10,5 @@ if TYPE_CHECKING:
 
 def get_requires(axiom: Axiom) -> Pattern:
     llvm_axiom = convert.sentence_to_llvm(axiom)
-    pattern = llvm_axiom.requires()
-    return convert.llvm_to_pattern(pattern)
+    llvm_pattern = llvm_axiom.requires
+    return convert.llvm_to_pattern(llvm_pattern)
